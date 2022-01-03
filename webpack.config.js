faef408360085b249f655600af35652ac2a07b0c
Dmitrii Suroviagin <savourygin@gmail.com>
2022-01-03 10:24:59 +0400

    ts-0: configure webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,32 @@
-"use strict";
+const path = require('path');
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    path: path.join(__dirname, 'dist'),
+    filename: "index-bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ],
   },
 };
