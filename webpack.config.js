b335c0a7dfffc8a488bda33fb5997408bdc68f0d
Dmitrii Suroviagin <savourygin@gmail.com>
2022-01-03 11:05:11 +0400

    ts-0: cofigure webpack to run the app in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  entry: "./src/index.js",
+const config = {
+  mode: isProd ? "production" : "development",
+  entry: "./src/index.tsx",
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, "dist"),
     filename: "index-bundle.js",
   },
   resolve: {
@@ -20,13 +24,35 @@ module.exports = {
         test: /\.ts$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
 };
+
+if (isProd) {
+  config.optimization = {
+    minimizer: [new TerserWebpackPlugin()],
+  };
+} else {
+  config.devServer = {
+    port: 9000,
+    open: true,
+    hot: true,
+    compress: true,
+  };
+}
+
+module.exports = config;
