c03d3d0dcf55f9e7fa42015d1fb2762a98ebc994
Dmitrii Suroviagin <savourygin@gmail.com>
2022-01-10 10:49:57 +0400

    ts-4: add scss rules for rendering icons

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from 'components/app';
 
+// Custom styles
+import 'styles/icons.scss';
+
 render(
   <React.StrictMode>
     <App />
