457417ad2bca4484aa1790acf21c5f365b5e3eb8
Dmitrii Suroviagin <savourygin@gmail.com>
2022-01-14 08:51:52 +0400

    ts-6: create the store function

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import AppRouter from 'components/app-router';
+import { Provider } from 'react-redux';
+import { store } from 'store';
 
 function App(): React.ReactElement {
-  return <AppRouter />;
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
 }
 
 export default App;
