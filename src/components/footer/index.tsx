2d3fa39d2c21c93c1f5bc1a934584cb770527812
Dmitrii Suroviagin <savourygin@gmail.com>
2022-01-15 13:18:27 +0400

    ts-7: fix classNames for the main layout

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import formatClassName from 'helpers/formatters/format-class-name';
 
-function Footer(): React.ReactElement {
-  return <footer className='footer'>Footer</footer>;
+export type FooterProps = {
+  className?: string;
+};
+
+function Footer(props: FooterProps): React.ReactElement {
+  return <footer className={formatClassName(['footer', props.className])}>Footer</footer>;
 }
 
 export default Footer;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useAppDispatch } from 'store/hooks';
 import { settingsActions } from 'store/settings/slice';
+import formatClassName from 'helpers/formatters/format-class-name';
 
-function Header(): React.ReactElement {
+export type HeaderProps = {
+  className?: string;
+};
+
+function Header(props: HeaderProps): React.ReactElement {
   const dispatch = useAppDispatch();
   const onClickMenuButton = () => {
     dispatch(settingsActions.toggleNavigation());
   };
 
   return (
-    <header className='header'>
+    <header className={formatClassName(['header', props.className])}>
       Header{' '}
       <button type='button' onClick={onClickMenuButton}>
         Menu
