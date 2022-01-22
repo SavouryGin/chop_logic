2f971f64cad2bcfa69c1f2f552833d43b93eabc7
Dmitrii Suroviagin <savourygin@gmail.com>
2022-01-22 10:24:28 +0400

    ts-10: add a general component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { useAppDispatch } from 'store/hooks';
 import { settingsActions } from 'store/settings/slice';
 import formatClassName from 'helpers/formatters/format-class-name';
+import { ClassNameProp } from 'types';
 
 import './styles.scss';
 
 export type HeaderProps = {
-  className?: string;
+  className?: ClassNameProp;
 };
 
 function Header(props: HeaderProps): React.ReactElement {
