e9226b94f00a2405c8c4ac274899f5a527715493
Dmitrii Suroviagin <savourygin@gmail.com>
2022-01-22 11:03:19 +0400

    ts-10: add tests for the button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import formatClassName from 'helpers/formatters/format-class-name';
 import { ClassNameProp } from 'types';
+import { Icon } from 'enums';
 
-type ButtonProps = {
-  type: 'button' | 'submit' | 'reset';
+export type ButtonProps = {
+  type?: 'button' | 'submit' | 'reset';
   text?: string;
   className?: ClassNameProp;
+  onClick?: () => void;
+  icon?: Icon;
 };
 
-function Button(props: ButtonProps) {
-  const { type, text, className } = props;
+function Button(props: ButtonProps): React.ReactElement {
+  const { type, text, className, onClick, icon } = props;
   return (
-    <button type={type} className={formatClassName(['button', className])}>
+    <button type={type || 'button'} className={formatClassName(['button', icon, className])} onClick={onClick}>
       {text}
     </button>
   );
