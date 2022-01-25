be4861107241c28866fcafbeb510cadcb42d2a71
Dmitrii Suroviagin <savourygin@gmail.com>
2022-01-25 08:10:42 +0400

    ts-12: add header sections

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -2,6 +2,15 @@ export enum Icon {
   Default = 'chop-icon__cross',
   Home = 'chop-icon__home',
   Menu = 'chop-icon__menu',
+  Enlarge = 'chop-icon__enlarge',
+  Shrink = 'chop-icon__shrink',
+  Settings = 'chop-icon__cog',
+  Delete = 'chop-icon__bin',
+  Up = 'chop-icon__circle-up',
+  Down = 'chop-icon__circle-down',
+  Right = 'chop-icon__circle-right',
+  Left = 'chop-icon__circle-left',
+  Test = 'chop-icon__arrow-right',
 }
 
 export enum Pages {
