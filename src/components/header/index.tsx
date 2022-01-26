4383cbfff2cb45e6def93c57e528629d3673a097
Dmitrii Suroviagin <savourygin@gmail.com>
2022-01-26 07:27:23 +0400

    ts-12: add right & left header panels

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Button from 'components/button';
-import { useAppDispatch } from 'store/hooks';
-import { settingsActions } from 'store/settings/slice';
 import formatClassName from 'helpers/formatters/format-class-name';
 import { ClassNameProp } from 'types';
-import { Icon } from 'enums';
-import { getIsNavigationOpen } from 'store/settings/selectors';
-import { useAppSelector } from 'store/hooks';
+import RightHeaderPanel from './right-panel';
+import LeftHeaderPanel from './left-panel';
 
 import './styles.scss';
 
@@ -15,22 +11,14 @@ export type HeaderProps = {
 };
 
 function Header(props: HeaderProps): React.ReactElement {
-  const dispatch = useAppDispatch();
-  const isNavigationOpen = useAppSelector(getIsNavigationOpen);
-  const onClickMenuButton = () => {
-    dispatch(settingsActions.toggleNavigation());
-  };
-
   return (
     <header className={formatClassName(['header', props.className])}>
       <div className='header__left-panel'>
-        <Button onClick={onClickMenuButton} icon={isNavigationOpen ? Icon.Default : Icon.Menu} />
+        <LeftHeaderPanel />
       </div>
       <h1 className='header__heading'>Chop Logic</h1>
       <div className='header__right-panel'>
-        <Button icon={Icon.Enlarge} />
-        <Button icon={Icon.Sound} />
-        <Button icon={Icon.LightMode} />
+        <RightHeaderPanel />
       </div>
     </header>
   );
