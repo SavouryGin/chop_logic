0eaa04a54b0373c0b9d1c94111fe5c789deed998
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-02 07:34:41 +0400

    ts-15: add footer content

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import formatClassName from 'helpers/formatters/format-class-name';
 import { ClassNameProp } from 'types';
+import { Icon } from 'enums';
 
 import './styles.scss';
 
@@ -11,7 +12,18 @@ export type FooterProps = {
 function Footer(props: FooterProps): React.ReactElement {
   return (
     <footer className={formatClassName(['footer', props.className])} data-testid='footer'>
-      Footer
+      <span className='footer__copyright'>© Dmitrii Suroviagin, 2022</span>
+      <span className='footer__links'>
+        <a href='mailto:savourygin@gmail.com' className={Icon.Mail}>
+          Mail
+        </a>
+        <a href='https://telegram.me/savoury_gin' target='_blank' rel='noreferrer' className={Icon.Telegram}>
+          Telegram
+        </a>
+        <a href='https://github.com/SavouryGin' target='_blank' rel='noreferrer' className={Icon.Github}>
+          GitHub
+        </a>
+      </span>
     </footer>
   );
 }
