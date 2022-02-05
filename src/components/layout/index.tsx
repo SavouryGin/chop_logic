634cf67f3154219efcf52b04722ab88f3ec97c42
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-05 10:42:04 +0400

    ts-16: add dark theme for the layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -8,20 +8,29 @@ import { settingsSelectors } from 'store/settings/selectors';
 import { useAppSelector } from 'store/hooks';
 
 import './styles.scss';
+import formatClassName from 'helpers/formatters/format-class-name';
 
 function Layout(): React.ReactElement {
   const isNavigationOpen = useAppSelector(settingsSelectors.getIsNavigationOpened);
   const isSidebarOpen = useAppSelector(settingsSelectors.getIsSidebarOpened);
+  const isDarkMode = useAppSelector(settingsSelectors.getIsDarkMode);
+
+  const layoutClassNames = formatClassName(['layout', { layout_dark: isDarkMode }]);
+  const navigationClassNames = formatClassName(['layout__navigation', { layout__navigation_dark: isDarkMode }]);
+  const sidebarClassNames = formatClassName(['layout__sidebar', { layout__sidebar_dark: isDarkMode }]);
+  const headerClassNames = formatClassName(['layout__header', { layout__header_dark: isDarkMode }]);
+  const mainClassNames = formatClassName(['layout__main', { layout__main_dark: isDarkMode }]);
+  const footerClassNames = 'layout__footer';
 
   return (
-    <div className='layout'>
-      {isNavigationOpen && <Navigation className='layout__navigation' />}
-      {isSidebarOpen && <Sidebar className='layout__sidebar' />}
-      <Header className='layout__header' />
-      <main className='layout__main'>
+    <div className={layoutClassNames}>
+      {isNavigationOpen && <Navigation className={navigationClassNames} />}
+      {isSidebarOpen && <Sidebar className={sidebarClassNames} />}
+      <Header className={headerClassNames} />
+      <main className={mainClassNames}>
         <Outlet />
       </main>
-      <Footer className='layout__footer' />
+      <Footer className={footerClassNames} />
     </div>
   );
 }
