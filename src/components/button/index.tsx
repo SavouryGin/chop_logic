18454d05bec8d0dcaf8571a30269bc45c0ba297d
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-06 18:09:54 +0400

    ts-16: add dark-theme colors for the button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import formatClassName from 'helpers/formatters/format-class-name';
 import { ClassNameProp } from 'types';
 import { Icon } from 'enums';
+import { settingsSelectors } from 'store/settings/selectors';
+import { useAppSelector } from 'store/hooks';
 
 import './styles.scss';
 
@@ -16,11 +18,17 @@ export type ButtonProps = {
 
 function Button(props: ButtonProps): React.ReactElement {
   const { type, text, className, onClick, icon, title } = props;
+  const isDarkMode = useAppSelector(settingsSelectors.getIsDarkMode);
+  const buttonClassNames = formatClassName(['button', className, { button_dark: isDarkMode }]);
+  const shadowClassNames = formatClassName(['button__shadow', { button__shadow_dark: isDarkMode }]);
+  const edgeClassNames = formatClassName(['button__edge', { button__edge_dark: isDarkMode }]);
+  const frontClassNames = formatClassName(['button__front', icon, { button__front_dark: isDarkMode }]);
+
   return (
-    <button type={type || 'button'} title={title} className={formatClassName(['button', className])} onClick={onClick}>
-      <span className='button__shadow'></span>
-      <span className='button__edge'></span>
-      <span className={formatClassName(['button__front', icon])}>{text}</span>
+    <button type={type || 'button'} title={title} className={buttonClassNames} onClick={onClick}>
+      <span className={shadowClassNames}></span>
+      <span className={edgeClassNames}></span>
+      <span className={frontClassNames}>{text}</span>
     </button>
   );
 }
