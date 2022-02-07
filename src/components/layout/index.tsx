0b88dd67954cc4dce09b62759cf4048ebc617ca3
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-07 07:30:22 +0400

    ts-17: refactor footer props

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -30,7 +30,7 @@ function Layout(): React.ReactElement {
       <main className={mainClassNames}>
         <Outlet />
       </main>
-      <Footer className={footerClassNames} />
+      <Footer className={footerClassNames} isDarkMode={isDarkMode} />
     </div>
   );
 }
