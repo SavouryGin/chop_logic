9785070b1ea5a6bde779f792b6c598a60f981054
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-09 07:35:57 +0400

    ts-18: refactor tests for the layout component

--- src/components/layout/__tests__/layout.test.tsx
+++ src/components/layout/__tests__/layout.test.tsx
@@ -50,4 +50,19 @@ describe('Layout component:', () => {
     userEvent.click(sidebarBtn);
     expect(screen.queryByRole('complementary')).toBeInTheDocument();
   });
+
+  it('the layout elements become dark if user clicks the Dark mode button', () => {
+    userEvent.click(screen.getByTitle(/navigation/i));
+    userEvent.click(screen.getByTitle(/sidebar/i));
+    expect(screen.getByTestId('layout')).not.toHaveClass('layout_dark');
+    expect(screen.getByRole('banner')).not.toHaveClass('layout__header_dark');
+    expect(screen.queryByRole('navigation')).not.toHaveClass('layout__navigation_dark');
+    expect(screen.queryByRole('complementary')).not.toHaveClass('layout__sidebar_dark');
+
+    userEvent.click(screen.getByTitle(/color theme/i));
+    expect(screen.getByTestId('layout')).toHaveClass('layout_dark');
+    expect(screen.getByRole('banner')).toHaveClass('layout__header_dark');
+    expect(screen.queryByRole('navigation')).toHaveClass('layout__navigation_dark');
+    expect(screen.queryByRole('complementary')).toHaveClass('layout__sidebar_dark');
+  });
 });
