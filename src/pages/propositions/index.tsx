7b2119fe417c764696b6e6211ff414d624aa89be
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-12 12:32:51 +0400

    ts-19: fix layout overflow styles

--- src/pages/propositions/index.tsx
+++ src/pages/propositions/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function Propositions(): React.ReactElement {
-  return <div className='propositions'>Propositions</div>;
+  const test =
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda, hic illo. Qui alias quasi repudiandae dignissimos unde, id sit molestiae ducimus numquam nihil, sint impedit molestias perferendis ipsa nobis reiciendis!';
+  return <div className='propositions'>{test.repeat(100)}</div>;
 }
 
 export default Propositions;
