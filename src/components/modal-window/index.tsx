3ff7c5775a3a2f85edd6ddf506693e36e68f7db6
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-18 08:08:45 +0400

    bug-4: fix animation for the modal window

--- src/components/modal-window/index.tsx
+++ src/components/modal-window/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Button from 'components/button';
 import formatClassName from 'helpers/formatters/format-class-name';
+
 import { ClassNameProp } from 'types';
 import { Icon } from 'enums';
 import { settingsSelectors } from 'store/settings/selectors';
@@ -23,26 +24,39 @@ function ModalWindow(props: ModalWindowProps): React.ReactElement | null {
   const targetElement = document.getElementById('modal');
   if (!isOpened || !targetElement) return null;
   const isDarkMode = useAppSelector(settingsSelectors.getIsDarkMode);
-  const windowClassNames = formatClassName(['modal-window', className, { 'modal-window_dark': isDarkMode }]);
   const backgroundClassNames = formatClassName(['modal-background', { 'modal-background_dark': isDarkMode }]);
   const contentClassNames = formatClassName(['modal-window__content', { 'modal-window__content_dark': isDarkMode }]);
+  const [windowClassNames, setWindowClassNames] = useState(
+    formatClassName(['modal-window', className, { 'modal-window_dark': isDarkMode }]),
+  );
 
-  const portal = (
-    <div className={backgroundClassNames}>
-      <div className={windowClassNames} role='dialog' aria-modal='true'>
-        <header className='modal-window__header' id='modal-window-heading'>
-          {title}
-        </header>
-        <div className={contentClassNames} role='region' aria-labelledby='modal-window-heading'>
-          {content}
-        </div>
-        <footer className='modal-window__footer'>
-          <Button onClick={onClose} icon={Icon.Cancel} title='Close' id='close-modal-window' />
-          {onConfirm && <Button onClick={onConfirm} icon={Icon.Default} title='Ok' />}
-        </footer>
+  const onClickClose = () => {
+    const classNamesToClose = `${windowClassNames} modal-window_closing`;
+    // wait for closing CSS animation
+    setWindowClassNames(classNamesToClose);
+    setTimeout(() => onClose(), 1000);
+  };
+
+  const buttons = (
+    <>
+      <Button onClick={onClickClose} icon={Icon.Cancel} title='Close' id='close-modal-window' />
+      {onConfirm && <Button onClick={onConfirm} icon={Icon.Default} title='Ok' />}
+    </>
+  );
+
+  const window = (
+    <div className={windowClassNames} role='dialog' aria-modal='true'>
+      <header className='modal-window__header' id='modal-window-heading'>
+        {title}
+      </header>
+      <div className={contentClassNames} role='region' aria-labelledby='modal-window-heading'>
+        {content}
       </div>
+      <footer className='modal-window__footer'>{buttons}</footer>
     </div>
   );
+
+  const portal = <div className={backgroundClassNames}>{window}</div>;
 
   return ReactDOM.createPortal(portal, targetElement);
 }
