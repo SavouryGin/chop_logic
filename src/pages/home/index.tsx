941ed980c55a7fd016d3b07f18d6eed8d0da31c3
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-20 15:30:00 +0400

    ts-20: add a stub input component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import TextInput from 'components/inputs/text-input';
 
 import './styles.scss';
 
 function Home(): React.ReactElement {
-  return <div className='home'>Home page</div>;
+  return (
+    <div className='home'>
+      Home page
+      <div>
+        <TextInput name='test-input' label='Type here:' isRequired />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
