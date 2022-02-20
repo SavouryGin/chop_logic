161d393ca7b2b4a8d92a0818f7cc6b4e7ca6f756
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-20 15:30:00 +0400

    ts-20: add a stub input component

--- src/components/inputs/text-input/index.tsx
+++ src/components/inputs/text-input/index.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import formatClassName from 'helpers/formatters/format-class-name';
+import { Guid } from 'guid-typescript';
 import { ComponentProps } from 'types';
 
 import './styles.scss';
 
 export type TextInputProps = ComponentProps & {
   name: string;
+  label: string;
+  placeholder?: string;
+  isDisabled?: boolean;
+  isRequired?: boolean;
+  maxLength?: number;
+  minLength?: number;
+  value?: string;
 };
 
 function TextInput(props: TextInputProps): React.ReactElement {
+  const { name, id, isDisabled, label } = props;
   const inputClassNames = formatClassName(['text-input', props.className]);
+  const inputId = id || Guid.create().toString();
+  const labelContent = (
+    <>
+      {label}
+      {props.isRequired && <abbr title='required'>*</abbr>}
+    </>
+  );
 
   return (
-    <input name={props.name} id={props.id} className={inputClassNames}>
-      Input
-    </input>
+    <div className={inputClassNames}>
+      <label htmlFor={inputId}>{labelContent}</label>
+      <input
+        type='text'
+        name={name}
+        id={inputId}
+        disabled={isDisabled}
+        maxLength={props.maxLength}
+        minLength={props.minLength}
+        value={props.value}
+        placeholder={props.placeholder || 'Please type...'}
+      />
+    </div>
   );
 }
 
