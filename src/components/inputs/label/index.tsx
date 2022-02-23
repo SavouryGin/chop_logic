44081df08883928f369562bd6538af9b33392f54
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-23 09:24:57 +0400

    ts-21: add styles for the label component

--- src/components/inputs/label/index.tsx
+++ src/components/inputs/label/index.tsx
@@ -3,6 +3,7 @@ import formatClassName from 'helpers/formatters/format-class-name';
 import { ComponentProps } from 'types';
 
 import './styles.scss';
+import { Icon } from 'enums';
 
 export type LabelProps = ComponentProps & {
   text: string;
@@ -14,15 +15,12 @@ export type LabelProps = ComponentProps & {
 function Label(props: LabelProps): React.ReactElement {
   const { text, isRequired, isDarkMode, className, inputId } = props;
   const labelClassNames = formatClassName(['label', className, { label_required: !!isRequired, label_dark: !!isDarkMode }]);
+  const requiredClassNames = formatClassName(['label__asterisk', Icon.Required, { label__asterisk_dark: !!isDarkMode }]);
 
   return (
     <label htmlFor={inputId} className={labelClassNames}>
       <span className='label__text'>{text}</span>
-      {isRequired && (
-        <abbr className='label_asterisk' title='required'>
-          *
-        </abbr>
-      )}
+      {isRequired && <abbr className={requiredClassNames} title='required'></abbr>}
     </label>
   );
 }
