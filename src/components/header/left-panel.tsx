316ba2fc48a2fcf3a1112334aa18cafe8925f21c
Dmitrii Suroviagin <savourygin@gmail.com>
2022-02-27 14:46:17 +0400

    ts-23: add the webpack config rule for sounds

--- src/components/header/left-panel.tsx
+++ src/components/header/left-panel.tsx
@@ -4,6 +4,7 @@ import { Icon } from 'enums';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
 import { settingsActions } from 'store/settings/slice';
 import { settingsSelectors } from 'store/settings/selectors';
+import { soundPlayer } from 'helpers/sounds';
 
 function LeftHeaderPanel(): React.ReactElement {
   const dispatch = useAppDispatch();
@@ -26,6 +27,8 @@ function LeftHeaderPanel(): React.ReactElement {
 
   const onClickModeButton = () => {
     dispatch(settingsActions.toggleFlag('isDarkMode'));
+    console.log(soundPlayer.collect);
+    soundPlayer.collect.play();
   };
 
   const onClickSoundsButton = () => {
