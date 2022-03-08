6ed1433b452b3ef6c8639c0e789202121b84e881
Dmitrii Suroviagin <savourygin@gmail.com>
2022-03-08 13:03:42 +0400

    ts-27: extend the select type

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,18 @@
 import Checkbox from 'components/inputs/checkbox';
 import Select from 'components/inputs/select';
 import TextInput from 'components/inputs/text-input';
+import { SelectEntity } from 'types';
 import React from 'react';
 
 import './styles.scss';
 
 function Home(): React.ReactElement {
+  const selectOptions: SelectEntity[] = [
+    { option: 'one', value: 1, add: 123 },
+    { option: 'two', value: 2, asdf: 'asdf' },
+    { option: 'three', value: 3, asdf: {} },
+  ];
+
   return (
     <div className='home'>
       Home page
@@ -18,16 +25,7 @@ function Home(): React.ReactElement {
       <br />
       <br />
       <div>
-        <Select
-          name='select1'
-          label='Label1'
-          options={[
-            { option: 'one', value: 1 },
-            { option: 'two', value: 2 },
-            { option: 'three', value: 3 },
-          ]}
-          defaultOption={{ option: 'three', value: 3 }}
-        />
+        <Select name='select1' label='Label1' options={selectOptions} defaultOption={selectOptions[2]} />
       </div>
       <br />
       <div>
