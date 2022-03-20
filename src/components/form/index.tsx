de420f34446172ade4460ffba17cdfdbaf398ac3
Dmitrii Suroviagin <savourygin@gmail.com>
2022-03-20 16:45:51 +0400

    ts-31: add form callbacks

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,6 +1,6 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import formatClassName from 'helpers/formatters/format-class-name';
-import { ComponentProps, IFormContextProps } from 'types';
+import { ComponentProps, FormContextProps, FormInput, FormValues } from 'types';
 import Button from 'components/button';
 import { Icon } from 'enums';
 import { soundPlayer } from 'helpers/sounds';
@@ -8,30 +8,32 @@ import { soundPlayer } from 'helpers/sounds';
 export type FormProps = ComponentProps & {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
   inputs: React.ReactElement;
-  initialValues: { [key: string]: unknown };
-  formRef?: React.MutableRefObject<HTMLFormElement | undefined>;
+  initialValues: FormValues;
   action?: string;
+  getValues?: (values: FormValues) => void;
 };
 
-export const FormContext = React.createContext({} as IFormContextProps);
+export const FormContext = React.createContext({} as FormContextProps);
 
 function Form(props: FormProps): React.ReactElement {
-  const { action, className, onSubmit, inputs, initialValues } = props;
+  const { action, className, onSubmit, inputs, initialValues, getValues } = props;
   const formClassNames = formatClassName(['form', className]);
   const [form, setForm] = useState(initialValues);
 
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    // Get the name of the field that caused this change event
-    // Get the new value of this field
-    const { name, value } = e.target;
-    console.log('form', e);
-    console.log(form);
-    // Update state
+  const onChangeInput = (e: React.ChangeEvent<FormInput>) => {
+    const isCheckbox = e.target instanceof HTMLInputElement && e.target.type == 'checkbox';
+    const name = e.target.name;
+    const value = isCheckbox ? (e.target as HTMLInputElement).checked : e.target.value;
+
     setForm({
       ...form,
       [name]: value,
     });
   };
+
+  useEffect(() => {
+    if (getValues) getValues(form);
+  }, [form]);
 
   return (
     <form className={formClassNames} action={action || '/'} onSubmit={onSubmit}>
