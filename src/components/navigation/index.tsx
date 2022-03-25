e114fd579379ca1f1e27206acb6662f3d18f41c6
Dmitrii Suroviagin <savourygin@gmail.com>
2022-03-25 07:54:17 +0400

    ts-34: add LocalText type and languages for nav

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import formatClassName from 'helpers/formatters/format-class-name';
 import AppLink from 'components/app-link';
 import { routesMap } from 'components/app-router/map';
-import { ComponentProps } from 'types';
+import { ComponentProps, LocalText } from 'types';
 import { useAppSelector } from 'hooks';
 import { settingsSelectors } from 'store/settings/selectors';
 
@@ -12,19 +12,21 @@ export type NavigationProps = ComponentProps;
 
 function Navigation(props: NavigationProps): React.ReactElement {
   const isClosingAnimationActive = useAppSelector(settingsSelectors.getIsMenuAnimationActive);
+  const language = useAppSelector(settingsSelectors.getLanguage);
+  const headerText: LocalText = { en: 'Navigation', ru: 'Навигация' };
   const navigationClassNames = formatClassName(['navigation', props.className, { navigation_closing: isClosingAnimationActive }]);
 
   const links = routesMap.map((item) => {
     return (
       <li key={item.key}>
-        <AppLink path={item.url} text={item.title} isNavigation icon={item.icon} />
+        <AppLink path={item.url} text={item.title[language]} isNavigation icon={item.icon} />
       </li>
     );
   });
 
   return (
     <nav className={navigationClassNames}>
-      <h2 className='navigation__header'>Navigation</h2>
+      <h2 className='navigation__header'>{headerText[language]}</h2>
       <ul className='navigation__list'>{links}</ul>
     </nav>
   );
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,3 +1,7 @@
+export type Language = 'ru' | 'en';
+
+export type LocalText = { [key in Language]: string };
+
 export type ClassNames = {
   [key in string]: boolean;
 };
