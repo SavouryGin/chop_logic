fe83f80d3e8e1b34a16247eab8e020168460f0da
Dmitrii Suroviagin <savourygin@gmail.com>
2022-04-01 08:48:21 +0400

    ts-37: add a draft table component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,16 @@
+import Table from 'components/table';
 import React from 'react';
 
 import './styles.scss';
 
 function Home(): React.ReactElement {
-  return <div className='home'>Home page</div>;
+  return (
+    <div className='home'>
+      Home page
+      <br></br>
+      <Table />
+    </div>
+  );
 }
 
 export default Home;
