de2283cc0177e158fcf2bd2fd458bcb39b573ac5
Dmitrii Suroviagin <savourygin@gmail.com>
2022-04-07 08:58:20 +0400

    ts-39: add tests for table helpers

--- src/components/table/helpers.ts
+++ src/components/table/helpers.ts
@@ -3,7 +3,7 @@ import { TableColumnProps, TableDataItem } from 'types/table';
 export function getDataCellsValues(tableRow: TableDataItem, columns: TableColumnProps[]): string[] {
   const values = [];
   for (const column of columns) {
-    const value = !column.field ? '' : (tableRow[column.field] as string);
+    const value = !column.field ? '' : (tableRow[column.field] as string).toString();
     values.push(value);
   }
 
