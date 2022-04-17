80ecf53e3d450a7314b5c1d22b0f4944a782934f
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-04-17 16:18:01 +0400

    ts-44: add tabs

--- src/pages/propositions/index.tsx
+++ src/pages/propositions/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import TabList from 'components/tab-list';
+import DirectProofsEditor from './elements/direct-proofs-editor';
+import NaturalProofsEditor from './elements/natural-proofs-editor';
+import DefinitionsOfPropositionalLogic from './elements/definitions-of-propositional-logic';
+import HeuristicsOfPropositionalLogic from './elements/heuristics-of-propositional-logic';
+import RulesOfInference from './elements/rules-of-inference';
+import { TabItem } from 'types';
 
 import './styles.scss';
 
 function Propositions(): React.ReactElement {
-  return <div className='propositions'></div>;
+  const tabs: TabItem[] = [
+    {
+      tabContent: <DirectProofsEditor />,
+      tabTitle: { en: 'Direct Proofs', ru: 'Прямые доказательства' },
+      tabId: 'direct-proofs-editor',
+    },
+    {
+      tabContent: <NaturalProofsEditor />,
+      tabTitle: { en: 'Natural Proofs', ru: 'Структурные доказательства' },
+      tabId: 'natural-proofs-editor',
+    },
+    {
+      tabContent: <DefinitionsOfPropositionalLogic />,
+      tabTitle: { en: 'Definitions', ru: 'Определения' },
+      tabId: 'definitions-of-propositional-logic',
+    },
+    {
+      tabContent: <HeuristicsOfPropositionalLogic />,
+      tabTitle: { en: 'Heuristics', ru: 'Эвристики' },
+      tabId: 'heuristics-of-propositional-logic',
+    },
+    {
+      tabContent: <RulesOfInference />,
+      tabTitle: { en: 'Rules', ru: 'Правила' },
+      tabId: 'rules-of-inference',
+    },
+  ];
+
+  return (
+    <div className='propositions'>
+      <TabList tabs={tabs} />
+    </div>
+  );
 }
 
 export default Propositions;
