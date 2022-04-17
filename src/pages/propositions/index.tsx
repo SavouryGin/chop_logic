68942c50bd8764b6c414b8ada90cd05b21ca0d6c
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-04-17 17:08:42 +0400

    ts-44: refactor styles

--- src/pages/propositions/index.tsx
+++ src/pages/propositions/index.tsx
@@ -1,47 +1,55 @@
 import React from 'react';
 import TabList from 'components/tab-list';
+import formatClassName from 'helpers/formatters/format-class-name';
 import DirectProofsEditor from './elements/direct-proofs-editor';
 import NaturalProofsEditor from './elements/natural-proofs-editor';
 import DefinitionsOfPropositionalLogic from './elements/definitions-of-propositional-logic';
 import HeuristicsOfPropositionalLogic from './elements/heuristics-of-propositional-logic';
 import RulesOfInference from './elements/rules-of-inference';
 import { TabItem } from 'types';
+import { Icon } from 'enums';
+import { pageTitle, tabTitles } from 'assets/texts/propositions';
+import { useAppSelector } from 'hooks';
+import { settingsSelectors } from 'store/settings/selectors';
 
 import './styles.scss';
 
 function Propositions(): React.ReactElement {
+  const language = useAppSelector(settingsSelectors.getLanguage);
+
   const tabs: TabItem[] = [
     {
       tabContent: <DirectProofsEditor />,
-      tabTitle: { en: 'Direct Proofs', ru: 'Прямые доказательства' },
+      tabTitle: tabTitles.direct,
       tabId: 'direct-proofs-editor',
     },
     {
       tabContent: <NaturalProofsEditor />,
-      tabTitle: { en: 'Natural Proofs', ru: 'Структурные доказательства' },
+      tabTitle: tabTitles.natural,
       tabId: 'natural-proofs-editor',
     },
     {
       tabContent: <DefinitionsOfPropositionalLogic />,
-      tabTitle: { en: 'Definitions', ru: 'Определения' },
+      tabTitle: tabTitles.definitions,
       tabId: 'definitions-of-propositional-logic',
     },
     {
       tabContent: <HeuristicsOfPropositionalLogic />,
-      tabTitle: { en: 'Heuristics', ru: 'Эвристики' },
+      tabTitle: tabTitles.heuristics,
       tabId: 'heuristics-of-propositional-logic',
     },
     {
       tabContent: <RulesOfInference />,
-      tabTitle: { en: 'Rules', ru: 'Правила' },
+      tabTitle: tabTitles.rules,
       tabId: 'rules-of-inference',
     },
   ];
 
   return (
-    <div className='propositions'>
-      <TabList tabs={tabs} />
-    </div>
+    <article className='propositions'>
+      <h2 className={formatClassName(['propositions__title', Icon.Propositions])}>{pageTitle[language]}</h2>
+      <TabList tabs={tabs} className='propositions__tabs' />
+    </article>
   );
 }
 
