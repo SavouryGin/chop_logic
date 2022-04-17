28966066fc92b52857a21459825ed0fa9568185a
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-04-17 17:08:42 +0400

    ts-44: refactor styles

---
+++ src/assets/texts/propositions.ts
@@ -0,0 +1,9 @@
+export const tabTitles = {
+  direct: { en: 'Direct Proofs', ru: 'Прямые доказательства' },
+  natural: { en: 'Natural Proofs', ru: 'Структурные доказательства' },
+  definitions: { en: 'Definitions', ru: 'Определения' },
+  heuristics: { en: 'Heuristics', ru: 'Эвристики' },
+  rules: { en: 'Rules', ru: 'Правила' },
+};
+
+export const pageTitle = { en: 'Propositional Logic', ru: 'Логика высказываний' };
