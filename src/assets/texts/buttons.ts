400ccb966adc3db0df34a3ed8b45eaf7615c917b
Dmitrii Suroviagin <savourygin@gmail.com>
2022-04-19 09:30:34 +0400

    ts-45: add buttons to the toolbar

--- src/assets/texts/buttons.ts
+++ src/assets/texts/buttons.ts
@@ -34,4 +34,36 @@ export const buttonTexts: ButtonText = {
     title: { en: 'Apply Settings', ru: 'Применить настройки' },
     innerText: { en: 'Apply', ru: 'Применить' },
   },
+  [ButtonID.Premise]: {
+    title: { en: 'Enter premise', ru: 'Ввести посылку' },
+    innerText: { en: 'Premise', ru: 'Посылка' },
+  },
+  [ButtonID.Reiteration]: {
+    title: { en: 'Reiterate proof step', ru: 'Повторить шаг доказательства' },
+    innerText: { en: 'Reiteration', ru: 'Повтор' },
+  },
+  [ButtonID.Replace]: {
+    title: { en: 'Replace symbol', ru: 'Заменить символ' },
+    innerText: { en: 'Replace', ru: 'Заменить' },
+  },
+  [ButtonID.Delete]: {
+    title: { en: 'Delete proof step', ru: 'Удалить шаг доказательства' },
+    innerText: { en: 'Delete', ru: 'Удалить' },
+  },
+  [ButtonID.ImplicationCreation]: {
+    title: { en: 'Implication Creation', ru: 'Введение импликации' },
+    innerText: { en: 'Implication Creation', ru: 'Введение импликации' },
+  },
+  [ButtonID.ImplicationDistribution]: {
+    title: { en: 'Implication Distribution', ru: 'Дистрибуция импликации' },
+    innerText: { en: 'Implication Distribution', ru: 'Дистрибуция импликации' },
+  },
+  [ButtonID.ImplicationReversal]: {
+    title: { en: 'Implication Reversal', ru: 'Обращение импликации' },
+    innerText: { en: 'Implication Reversal', ru: 'Обращение импликации' },
+  },
+  [ButtonID.ImplicationElimination]: {
+    title: { en: 'Implication Elimination', ru: 'Удаление импликации' },
+    innerText: { en: 'Implication Elimination', ru: 'Удаление импликации' },
+  },
 };
--- src/pages/propositions/elements/direct-proofs-editor/elements/direct-proofs-editor-toolbar.tsx
+++ src/pages/propositions/elements/direct-proofs-editor/elements/direct-proofs-editor-toolbar.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Button from 'components/button';
+import { ButtonID } from 'enums';
+import { soundPlayer } from 'helpers/sounds';
 
 function DirectProofsEditorToolbar(): React.ReactElement {
-  return <div className='direct-proofs-editor__toolbar'>DirectProofsEditorToolbar</div>;
+  return (
+    <div className='direct-proofs-editor__toolbar'>
+      <Button buttonId={ButtonID.Premise} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.Reiteration} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.Replace} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.Delete} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.ImplicationCreation} sound={soundPlayer.slideClick} size='large' />
+      <Button buttonId={ButtonID.ImplicationDistribution} sound={soundPlayer.slideClick} size='large' />
+      <Button buttonId={ButtonID.ImplicationReversal} sound={soundPlayer.slideClick} size='large' />
+      <Button buttonId={ButtonID.ImplicationElimination} sound={soundPlayer.slideClick} size='large' />
+    </div>
+  );
 }
 
 export default DirectProofsEditorToolbar;
