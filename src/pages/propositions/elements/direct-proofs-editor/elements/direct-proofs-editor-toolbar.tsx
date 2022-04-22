72a4cde13f43a404179ef4773cdf6362362167b1
Dmitrii Suroviagin <savourygin@gmail.com>
2022-04-22 13:47:52 +0300

    ts-56: add a dispatcher for the DPE Toolbar

--- src/pages/propositions/elements/direct-proofs-editor/elements/direct-proofs-editor-toolbar.tsx
+++ src/pages/propositions/elements/direct-proofs-editor/elements/direct-proofs-editor-toolbar.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import Button from 'components/button';
 import { ButtonID } from 'enums';
 import { soundPlayer } from 'helpers/sounds';
+import { useAppDispatch } from 'hooks';
+import { propositionsActions as actions } from 'store/propositions/slice';
 
 function DirectProofsEditorToolbar(): React.ReactElement {
+  const dispatch = useAppDispatch();
+
+  const onClickPremise = () => {
+    dispatch(actions.setUpFlag({ flag: 'isPremiseOpened', value: true }));
+  };
+
   return (
     <div className='direct-proofs-editor__toolbar'>
-      <Button buttonId={ButtonID.Premise} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.Premise} sound={soundPlayer.keyboard} size='large' onClick={onClickPremise} />
       <Button buttonId={ButtonID.Reiteration} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.Replace} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.Delete} sound={soundPlayer.keyboard} size='large' />
