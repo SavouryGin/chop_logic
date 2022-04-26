ca4b53e4a0323393e0bd7108caafbe6e291e5084
Dmitrii Suroviagin <savourygin@gmail.com>
2022-04-26 07:23:11 +0200

    bug-5: fix tests

--- src/components/table/__tests__/helpers.test.ts
+++ src/components/table/__tests__/helpers.test.ts
@@ -3,17 +3,17 @@ import { getDataCellsValues } from '../helpers';
 
 describe('getDataCellsValues() tests:', () => {
   it('returns a string array from the first row', () => {
-    const result = getDataCellsValues(testTableData[0], testTableColumns);
+    const result = getDataCellsValues(testTableData[0], testTableColumns, 'en');
     expect(result).toEqual(['1', '2', '3', '4']);
   });
 
   it('returns a string array from the second row', () => {
-    const result = getDataCellsValues(testTableData[1], testTableColumns);
+    const result = getDataCellsValues(testTableData[1], testTableColumns, 'en');
     expect(result).toEqual(['5', '6', '7', '8']);
   });
 
   it('returns an empty array if an empty array is given', () => {
-    const result = getDataCellsValues(testTableData[1], []);
+    const result = getDataCellsValues(testTableData[1], [], 'en');
     expect(result).toEqual([]);
   });
 });
