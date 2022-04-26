7d69d97d0aeca21f891e5fd50112ad347de2849f
Dmitrii Suroviagin <savourygin@gmail.com>
2022-04-26 07:39:56 +0200

    bug-5: add a test for  getDataCellsValues()

--- src/__mocks__/test-data/table.ts
+++ src/__mocks__/test-data/table.ts
@@ -1,15 +1,15 @@
 import { TableColumnProps, TableDataItem } from 'types/table';
 
 export const testTableColumns: TableColumnProps[] = [
-  { title: 'Col 1', field: 'field1' },
-  { title: 'Col 2', field: 'field2' },
-  { title: 'Col 3', field: 'field3' },
-  { title: 'Col 4', field: 'field4' },
+  { title: { en: 'Col 1', ru: 'Кол1' }, field: 'field1' },
+  { title: { en: 'Col 2', ru: 'Кол2' }, field: 'field2' },
+  { title: { en: 'Col 3', ru: 'Кол3' }, field: 'field3' },
+  { title: { en: 'Col 4', ru: 'Кол4' }, field: 'field4' },
 ];
 
 export const testTableData: TableDataItem[] = [
-  { id: 'row1', field1: 1, field2: 2, field3: 3, field4: 4 },
-  { id: 'row2', field1: 5, field2: 6, field3: 7, field4: 8 },
-  { id: 'row3', field1: 9, field2: 10, field3: 11, field4: 12 },
-  { id: 'row4', field1: 13, field2: 14, field3: 15, field4: 16 },
+  { id: 'row1', field1: 1, field2: 2, field3: 3, field4: { en: 'en4', ru: 'ru4' } },
+  { id: 'row2', field1: 5, field2: 6, field3: 7, field4: { en: 'en8', ru: 'ru8' } },
+  { id: 'row3', field1: 9, field2: 10, field3: 11, field4: { en: 'en12', ru: 'ru12' } },
+  { id: 'row4', field1: 13, field2: 14, field3: 15, field4: { en: 'en16', ru: 'ru16' } },
 ];
