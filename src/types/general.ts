560eba561d25d079de61df16c1a8206a42d10c50
Dmitrii Suroviagin <savourygin@gmail.com>
2022-05-01 21:03:08 +0200

    ts-49: add TextContainer type

--- src/types/general.ts
+++ src/types/general.ts
@@ -8,6 +8,8 @@ export type Language = 'ru' | 'en';
 
 export type LocalText = { [key in Language]: string };
 
+export type TextContainer = { [key in string]: LocalText };
+
 export type ClassNames = {
   [key in string]: boolean;
 };
