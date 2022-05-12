8fea3d8c9c3ca30be5be85895ce2bd56c156ecf0
Dmitrii Suroviagin <savourygin@gmail.com>
2022-05-12 08:34:38 +0200

    ts-54: add helpers functions

--- src/helpers/converters/propositions-converter.ts
+++ src/helpers/converters/propositions-converter.ts
@@ -1,14 +1,64 @@
 import { PropositionalOperator } from 'enums';
-import { PropositionalExpression, PropositionalFormula } from 'types';
+import { PropositionalExpression, PropositionalFormula, PropositionalSymbol } from 'types';
 
 abstract class PropositionsConverter {
   public static convertExpressionToFormula(input: PropositionalExpression): PropositionalFormula {
-    console.log(input);
+    console.log(PropositionsConverter.addPropositionalAtomsToExpression(input));
 
     return {
       operator: PropositionalOperator.Var,
       values: 'P',
     };
+  }
+
+  public static addPropositionalAtomsToExpression(input: PropositionalExpression): Array<PropositionalSymbol | PropositionalFormula> {
+    return input.map((item) => (item.type === 'variable' ? PropositionsConverter.createPropositionalAtomFrom(item) : item));
+  }
+
+  public static createPropositionalAtomFrom(symbol: PropositionalSymbol): PropositionalFormula {
+    return {
+      operator: PropositionalOperator.Var,
+      values: symbol.representation || symbol.input.toLocaleUpperCase(),
+    };
+  }
+
+  public static createImplication(firstArgument: PropositionalFormula, secondArgument: PropositionalFormula): PropositionalFormula {
+    return {
+      operator: PropositionalOperator.Implies,
+      values: [firstArgument, secondArgument],
+    };
+  }
+
+  public static createConjunction(firstArgument: PropositionalFormula, secondArgument: PropositionalFormula): PropositionalFormula {
+    return {
+      operator: PropositionalOperator.And,
+      values: [firstArgument, secondArgument],
+    };
+  }
+
+  public static createDisjunction(firstArgument: PropositionalFormula, secondArgument: PropositionalFormula): PropositionalFormula {
+    return {
+      operator: PropositionalOperator.Or,
+      values: [firstArgument, secondArgument],
+    };
+  }
+
+  public static createEquivalence(firstArgument: PropositionalFormula, secondArgument: PropositionalFormula): PropositionalFormula {
+    return {
+      operator: PropositionalOperator.Equiv,
+      values: [firstArgument, secondArgument],
+    };
+  }
+
+  public static createNegation(argument: PropositionalFormula): PropositionalFormula {
+    return {
+      operator: PropositionalOperator.Not,
+      values: [argument],
+    };
+  }
+
+  public static isPropositionalAtom(input: PropositionalFormula): boolean {
+    return input.operator === PropositionalOperator.Var && typeof input.values === 'string';
   }
 }
 
