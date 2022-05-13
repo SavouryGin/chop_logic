180866c9cd94917023796de1ac4b40f81a7597fc
Dmitrii Suroviagin <savourygin@gmail.com>
2022-05-13 11:53:43 +0200

    ts-54: add more helper methods

--- src/helpers/converters/propositions-converter.ts
+++ src/helpers/converters/propositions-converter.ts
@@ -1,16 +1,38 @@
 import { PropositionalOperator } from 'enums';
 import { IncorrectPropositionalFormula } from 'errors/incorrect-propositional-formula';
+import PropositionsParser from 'helpers/parsers/propositions-parser';
 import { PropositionalExpression, PropositionalFormula, PropositionalSymbol } from 'types';
 
 abstract class PropositionsConverter {
   public static convertExpressionToFormula(input: PropositionalExpression): PropositionalFormula {
     console.log(PropositionsConverter.addPropositionalAtomsToExpression(input));
-    if (input[0].input !== '(') throw IncorrectPropositionalFormula;
+    // if (input[0].input !== '(') throw IncorrectPropositionalFormula;
+
+    PropositionsConverter.extractSubExpressions(input);
 
     return {
       operator: PropositionalOperator.Var,
       values: 'P',
     };
+  }
+
+  public static extractSubExpressions(expression: PropositionalExpression): any {
+    const result: any[] = [];
+    const openParenthesisIndexes = PropositionsConverter.getAllIndexesOfSymbol(expression, '(');
+    const closeParenthesisIndexes = PropositionsConverter.getAllIndexesOfSymbol(expression, ')');
+
+    console.log('openParenthesisIndexes', openParenthesisIndexes);
+    console.log('openParenthesisIndexes', closeParenthesisIndexes);
+
+    return result;
+  }
+
+  public static getAllIndexesOfSymbol(array: PropositionalExpression, symbol: string) {
+    const indexes = [];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].input === symbol) indexes.push(i);
+    }
+    return indexes;
   }
 
   public static addPropositionalAtomsToExpression(input: PropositionalExpression): Array<PropositionalSymbol | PropositionalFormula> {
@@ -57,6 +79,13 @@ abstract class PropositionsConverter {
       operator: PropositionalOperator.Not,
       values: [argument],
     };
+  }
+
+  public static isAtomicExpression(expression: PropositionalExpression) {
+    for (const symbol of expression) {
+      if (PropositionsParser.logicalOperators.includes(symbol.input)) return false;
+    }
+    return true;
   }
 
   public static isPropositionalAtom(input: PropositionalFormula): boolean {
