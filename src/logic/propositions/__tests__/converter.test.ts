f6bce7fc17cb0d25c4d1c4dbbbc1298b7bec9e41
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-06-01 11:13:47 +0200

    ts-58: add converter tests

--- src/logic/propositions/__tests__/converter.test.ts
+++ src/logic/propositions/__tests__/converter.test.ts
@@ -1,5 +1,6 @@
 import converter from '../converter';
 import testData from '__mocks__/test-data/propositions';
+import { PropositionalError } from 'errors/propositional-error';
 
 describe('Propositions converter tests', () => {
   it('convertStringToExpression() method returns a correct propositional expression', () => {
@@ -16,5 +17,20 @@ describe('Propositions converter tests', () => {
     expect(converter.convertExpressionToFormula(testData.propositionalExpression)).toEqual(testData.propositionalFormula);
     expect(converter.convertExpressionToFormula(testData.firstSubExpression)).toEqual(testData.firstSubFormula);
     expect(converter.convertExpressionToFormula(testData.secondSubExpression)).toEqual(testData.secondSubFormula);
+  });
+
+  it('convertExpressionToFormula() method throws an error if the main symbol is incorrect', () => {
+    expect(() => {
+      converter.convertExpressionToFormula([
+        testData.openParenthesis,
+        {
+          input: '5',
+          representation: '5',
+          type: 'parentheses',
+          position: 1,
+        },
+        testData.closeParenthesis,
+      ]);
+    }).toThrow(PropositionalError);
   });
 });
