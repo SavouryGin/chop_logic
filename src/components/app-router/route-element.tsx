f4ea0d74f927f0bf7b480e750169a0cd2d469894
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-06-20 20:53:47 +0200

    ts-61: refactor app components

--- src/components/app-router/route-element.tsx
+++ src/components/app-router/route-element.tsx
@@ -1,12 +1,12 @@
 import FallbackPage from 'pages/fallback-page';
 import React, { Suspense } from 'react';
 
-function RouteElement({ component: Component }: { component: React.FC }) {
+const RouteElement = ({ component: Component }: { component: React.FC }) => {
   return (
     <Suspense fallback={<FallbackPage />}>
       <Component />
     </Suspense>
   );
-}
+};
 
 export default RouteElement;
--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from 'store';
 
-function App(): React.ReactElement {
+const App = () => {
   return (
     <Provider store={store}>
       <AppRouter />
     </Provider>
   );
-}
+};
 
 export default App;
