7e76a844ffbe0582156235963995089fa8e69041
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-07-06 08:23:55 +0200

    ts-64: add a preview to the IC form

--- src/components/propositions/implication-creation-form/index.tsx
+++ src/components/propositions/implication-creation-form/index.tsx
@@ -1,9 +1,11 @@
 import Form from 'components/controls/form';
+import FormulaPreview from 'components/controls/formula-preview';
 import React, { useState } from 'react';
 import TextInput from 'components/controls/text-input';
 import constants from 'presets/propositions';
 import { ButtonID, GreekSymbol, InputID, LogicalSymbolHexCode } from 'enums';
 import { FormValues } from 'types';
+import { PropositionalError } from 'errors/propositional-error';
 import { closePropositionsPopup } from 'pages/propositions/elements/direct-proofs-editor/helpers';
 import { formsTexts } from 'texts/propositions';
 import { propositionsActions } from 'store/propositions/slice';
@@ -14,8 +16,14 @@ import './styles.scss';
 const ImplicationCreationForm = () => {
   const dispatch = useAppDispatch();
   const [values, setValues] = useState(constants.implicationCreationInitialValues);
-  const isDisabled = values.firstVariable.length === 0 || values.secondVariable.length === 0;
+  const [formError, setFormError] = useState<PropositionalError | null>(null);
   const language = useAppSelector(settingsSelectors.getLanguage);
+
+  const isFormEmpty = !values.firstVariable || !values.secondVariable;
+  const isFormInvalid = !!formError || isFormEmpty;
+  const previewString = isFormEmpty ? '' : `${values.firstVariable} => (${values.secondVariable} => ${values.firstVariable})`;
+
+  const takeError = (err: PropositionalError | null) => setFormError(err);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -29,6 +37,7 @@ const ImplicationCreationForm = () => {
     <>
       <TextInput name='firstVariable' inputId={InputID.FirstMetaVariable} className='implication-creation-form__input' />
       <TextInput name='secondVariable' inputId={InputID.SecondMetaVariable} className='implication-creation-form__input' />
+      <FormulaPreview text={previewString} passError={takeError} />
     </>
   );
 
@@ -42,7 +51,7 @@ const ImplicationCreationForm = () => {
         inputs={content}
         submitButtonId={ButtonID.ApplySettings}
         passValues={takeValues}
-        isSubmitDisabled={isDisabled}
+        isSubmitDisabled={isFormInvalid}
       />
     </div>
   );
