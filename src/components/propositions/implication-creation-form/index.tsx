ff10e4924336c3d922775053fbdbbab9779fd189
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-07-09 10:37:25 +0200

    ts-64: implement new hooks

--- src/components/propositions/implication-creation-form/index.tsx
+++ src/components/propositions/implication-creation-form/index.tsx
@@ -5,25 +5,25 @@ import TextInput from 'components/controls/text-input';
 import constants from 'presets/propositions';
 import { ButtonID, InputID } from 'enums';
 import { FormValues } from 'types';
-import { PropositionalError } from 'errors/propositional-error';
 import { closePropositionsPopup } from 'pages/propositions/elements/direct-proofs-editor/helpers';
 import { formsTexts } from 'texts/propositions';
 import { propositionsActions } from 'store/propositions/slice';
 import { settingsSelectors } from 'store/settings/selectors';
-import { useAppDispatch, useAppSelector } from 'hooks';
+import { useAppDispatch, useAppSelector, useImplicationCreationPreview } from 'hooks';
 import './styles.scss';
 
 const ImplicationCreationForm = () => {
   const dispatch = useAppDispatch();
   const [values, setValues] = useState(constants.implicationCreationInitialValues);
-  const [formError, setFormError] = useState<PropositionalError | null>(null);
   const language = useAppSelector(settingsSelectors.getLanguage);
 
-  const isFormEmpty = !values.firstVariable || !values.secondVariable;
-  const isFormInvalid = !!formError || isFormEmpty;
-  const previewString = isFormEmpty ? '' : `${values.firstVariable} => (${values.secondVariable} => ${values.firstVariable})`;
-
-  const takeError = (err: PropositionalError | null) => setFormError(err);
+  // const isFormEmpty = !values.firstVariable || !values.secondVariable;
+  // const isFormInvalid = !!formError || isFormEmpty;
+  // const previewString = isFormEmpty ? '' : `${values.firstVariable} => (${values.secondVariable} => ${values.firstVariable})`;
+  const preview = useImplicationCreationPreview(values.firstVariable, values.secondVariable);
+  const hasError = !Array.isArray(preview);
+  const isEmpty = !values.firstVariable || !values.secondVariable;
+  const isFormInvalid = hasError || isEmpty;
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -37,7 +37,7 @@ const ImplicationCreationForm = () => {
     <>
       <TextInput name='firstVariable' inputId={InputID.FirstMetaVariable} className='implication-creation-form__input' />
       <TextInput name='secondVariable' inputId={InputID.SecondMetaVariable} className='implication-creation-form__input' />
-      <FormulaPreview text={previewString} passError={takeError} />
+      <FormulaPreview preview={preview} />
     </>
   );
 
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,2 @@
-import { AppDispatch, RootState } from 'types';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export * from './common';
+export * from './propositions';
