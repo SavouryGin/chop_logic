85c9fbec7014530ac757dba3cc2f81bb80052dfb
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-07-10 20:30:34 +0200

    ts-65: add draft components

---
+++ src/components/propositions/implication-reversal-form/index.tsx
@@ -0,0 +1,8 @@
+import React, { memo } from 'react';
+import './styles.scss';
+
+const ImplicationReversalForm = () => {
+  return <div className='implication-reversal-form'>Hello</div>;
+};
+
+export default memo(ImplicationReversalForm);
