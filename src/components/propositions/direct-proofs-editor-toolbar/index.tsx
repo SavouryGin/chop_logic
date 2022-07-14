cb783b3fe5b9bea029f4b72901b3e7a949f43d3b
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-07-14 09:33:23 +0200

    ts-66: add custom hook for enabling IE

--- src/components/propositions/direct-proofs-editor-toolbar/index.tsx
+++ src/components/propositions/direct-proofs-editor-toolbar/index.tsx
@@ -4,13 +4,14 @@ import { ButtonID } from 'enums';
 import { propositionsActions as actions } from 'store/propositions/slice';
 import { propositionsSelectors } from 'store/propositions/selectors';
 import { soundPlayer } from 'helpers/sounds';
-import { useAppDispatch, useAppSelector } from 'hooks';
+import { useAppDispatch, useAppSelector, useImplicationEliminationEnabling } from 'hooks';
 
 const DirectProofsEditorToolbar = () => {
   const dispatch = useAppDispatch();
   const selectedIds = useAppSelector(propositionsSelectors.getSelectedIds);
   const isReiterationDisabled = selectedIds.length !== 1;
   const isDeleteDisabled = selectedIds.length === 0;
+  const isImplicationEliminationEnabled = useImplicationEliminationEnabling();
 
   const deleteSteps = () => {
     dispatch(actions.deleteSteps());
@@ -51,7 +52,12 @@ const DirectProofsEditorToolbar = () => {
       <Button buttonId={ButtonID.ImplicationCreation} sound={soundPlayer.slideClick} size='large' onClick={openIC} />
       <Button buttonId={ButtonID.ImplicationDistribution} sound={soundPlayer.slideClick} size='large' onClick={openID} />
       <Button buttonId={ButtonID.ContradictionRealization} sound={soundPlayer.slideClick} size='large' onClick={openCR} />
-      <Button buttonId={ButtonID.ImplicationElimination} sound={soundPlayer.slideClick} size='large' />
+      <Button
+        buttonId={ButtonID.ImplicationElimination}
+        sound={soundPlayer.slideClick}
+        isDisabled={!isImplicationEliminationEnabled}
+        size='large'
+      />
     </div>
   );
 };
