7aec8bcecffc825b5a3e290f5108ac6e5bc22a29
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-07-15 21:30:47 +0200

    ts-66: add getImplicationEliminationConsequent()

--- src/logic/propositions/converter.ts
+++ src/logic/propositions/converter.ts
@@ -184,6 +184,42 @@ const converter = {
 
     return output;
   },
+
+  getImplicationEliminationConsequent(firstFormula: PropositionalFormula, secondFormula: PropositionalFormula): PropositionalFormula {
+    const isFirstImplication = firstFormula.operator === PropositionalOperator.Implies && firstFormula.values.length === 2;
+    const isSecondImplication = secondFormula.operator === PropositionalOperator.Implies && secondFormula.values.length === 2;
+
+    if (isFirstImplication && !isSecondImplication) {
+      const antecedent = firstFormula.values[0] as PropositionalFormula;
+      const consequent = firstFormula.values[1] as PropositionalFormula;
+
+      if (validator.areTwoFormulasEqual(antecedent, secondFormula)) {
+        return consequent;
+      }
+    }
+
+    if (!isFirstImplication && isSecondImplication) {
+      const antecedent = secondFormula.values[0] as PropositionalFormula;
+      const consequent = secondFormula.values[1] as PropositionalFormula;
+
+      if (validator.areTwoFormulasEqual(antecedent, firstFormula)) {
+        return consequent;
+      }
+    }
+
+    const firstAntecedent = firstFormula.values[0] as PropositionalFormula;
+    const firstConsequent = firstFormula.values[1] as PropositionalFormula;
+    const secondAntecedent = secondFormula.values[0] as PropositionalFormula;
+    const secondConsequent = secondFormula.values[1] as PropositionalFormula;
+
+    if (validator.areTwoFormulasEqual(firstAntecedent, secondFormula)) {
+      return firstConsequent;
+    } else if (validator.areTwoFormulasEqual(secondAntecedent, firstFormula)) {
+      return secondConsequent;
+    } else {
+      throw new PropositionalError('Cannot perform the implication elimination.', errorsTexts.semanticError);
+    }
+  },
 };
 
 export default Object.freeze(converter);
