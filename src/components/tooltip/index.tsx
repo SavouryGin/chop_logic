897c481373d910a9d9e7bb6d054167839a54b8e0
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-07-24 09:19:01 +0200

    ts-67: remove logs & add types

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import formatClassName from 'helpers/formatters/format-class-name';
-import { CommonProps } from 'types';
+import { TooltipProps } from 'types';
 import { settingsSelectors } from 'store/settings/selectors';
 import { useAppSelector, useHover } from 'hooks';
 import './styles.scss';
-
-type TooltipProps = CommonProps & {
-  text: string;
-  position?: 'top' | 'bottom';
-  children?: React.ReactElement | string;
-};
 
 const Tooltip = ({ text, children, position = 'bottom' }: TooltipProps) => {
   const [hoverRef, isHovered] = useHover<HTMLDivElement>();
   const isDarkMode = useAppSelector(settingsSelectors.getIsDarkMode);
   const tooltipClass = formatClassName(['tooltip', { tooltip_dark: isDarkMode }]);
   const containerClass = formatClassName(['tooltip__container', `tooltip__container_${position}`]);
-  console.log(isHovered);
+
   const tooltipContainer = <span className={containerClass}>{text}</span>;
 
   return (
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import Tooltip from 'components/tooltip';
 
 import './styles.scss';
 
 const Home = () => {
-  return (
-    <div className='home'>
-      Home page
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Tooltip
-        text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt necessitatibus, maxime dolores impedit
-          minus consequatur vero vitae accusantium nulla error, eaque harum excepturi sequi consequuntur pariatur, fugit architecto a
-          dignissimos?'
-      >
-        <span>Test div for tooltip</span>
-      </Tooltip>
-      <br></br>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium consectetur cupiditate omnis deleniti temporibus dolore alias
-        ducimus corporis repellat ullam asperiores ab vel magni ratione saepe, expedita porro pariatur ex?
-      </p>
-    </div>
-  );
+  return <div className='home'>Home page</div>;
 };
 
 export default Home;
