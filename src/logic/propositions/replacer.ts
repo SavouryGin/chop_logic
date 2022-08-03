b9e5eb86e7f43b1ed88a60bf4c1758f36c17e203
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-08-03 20:20:31 +0200

    ts-68: add new replacer methods

--- src/logic/propositions/replacer.ts
+++ src/logic/propositions/replacer.ts
@@ -1,4 +1,6 @@
+import converter from './converter';
 import { DirectProofsTableItem } from 'store/propositions/interfaces';
+import { PropositionalExpression, PropositionalSymbol } from 'types';
 
 const replacer = {
   replacePropositionalVariableInTableItems(
@@ -12,10 +14,44 @@ const replacer = {
   },
 
   replacePropositionalVariableInItem(item: DirectProofsTableItem, newVariable: string, oldVariable: string): DirectProofsTableItem {
-    console.log(newVariable, oldVariable);
+    const newExpression = this.replaceVariableInPropositionalExpression(item.expression, newVariable, oldVariable);
+    const newFormula = converter.convertExpressionToFormula(newExpression);
+    const newFriendlyExpression = converter.convertFormulaToUserFriendlyExpression(newFormula);
 
     return {
       ...item,
+      rawInput: this.replaceVariableInRawInput(item.rawInput, newVariable, oldVariable),
+      expression: newExpression,
+      formula: newFormula,
+      friendlyExpression: newFriendlyExpression,
+    };
+  },
+
+  replaceVariableInRawInput(input: string, newVariable: string, oldVariable: string): string {
+    return input.replaceAll(oldVariable, newVariable);
+  },
+
+  replaceVariableInPropositionalExpression(
+    input: PropositionalExpression,
+    newVariable: string,
+    oldVariable: string,
+  ): PropositionalExpression {
+    return input.map((symbol) => this.replaceVariableInPropositionalSymbol(symbol, newVariable, oldVariable));
+  },
+
+  replaceVariableInPropositionalSymbol(symbol: PropositionalSymbol, newVariable: string, oldVariable: string): PropositionalSymbol {
+    if (symbol.type !== 'variable') {
+      return symbol;
+    }
+
+    if (symbol.input.toUpperCase() !== oldVariable.toUpperCase()) {
+      return symbol;
+    }
+
+    return {
+      ...symbol,
+      input: newVariable,
+      representation: newVariable.toUpperCase(),
     };
   },
 };
