790c00dc7f71ad7421a49e677d053e35c5cc7a35
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-08-08 20:38:00 +0200

    ts-70: add draft natural proofs components

---
+++ src/components/propositions/tables/natural-proofs/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NaturalProofsEditorTable = () => {
+  return <div className='natural-proofs-editor__table'>Table</div>;
+};
+
+export default NaturalProofsEditorTable;
