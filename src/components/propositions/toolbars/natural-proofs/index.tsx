570b65c976536e993a3c811c3eb401d60d1e8606
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-08-09 15:57:22 +0200

    ts-70: change styles for the toolbar

--- src/components/propositions/toolbars/natural-proofs/index.tsx
+++ src/components/propositions/toolbars/natural-proofs/index.tsx
@@ -7,18 +7,18 @@ const NaturalProofsEditorToolbar = () => {
   return (
     <div className='natural-proofs-editor__toolbar'>
       <Button buttonId={ButtonID.Premise} sound={soundPlayer.keyboard} size='large' />
-      <Button buttonId={ButtonID.Assumption} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.Reiteration} sound={soundPlayer.keyboard} size='large' />
-      <Button buttonId={ButtonID.Delete} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.NotIntroduction} sound={soundPlayer.keyboard} size='large' />
-      <Button buttonId={ButtonID.NotElimination} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.AndIntroduction} sound={soundPlayer.keyboard} size='large' />
-      <Button buttonId={ButtonID.AndElimination} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.OrIntroduction} sound={soundPlayer.keyboard} size='large' />
-      <Button buttonId={ButtonID.OrElimination} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.ImpliesIntroduction} sound={soundPlayer.keyboard} size='large' />
-      <Button buttonId={ButtonID.ImpliesElimination} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.EquivIntroduction} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.Assumption} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.Delete} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.NotElimination} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.AndElimination} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.OrElimination} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.ImpliesElimination} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.EquivElimination} sound={soundPlayer.keyboard} size='large' />
     </div>
   );
