028895a90cc2740de31781f369fdc41baa683920
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-08-10 20:34:04 +0200

    ts-70: add a draft table component

--- src/components/propositions/tables/natural-proofs/index.tsx
+++ src/components/propositions/tables/natural-proofs/index.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Table from 'components/table';
+import constants from 'presets/propositions';
+import { propositionsActions as actions } from 'store/propositions/slice';
+import { fillerText } from 'texts';
+import { propositionsSelectors } from 'store/propositions/selectors';
+import { settingsSelectors } from 'store/settings/selectors';
+import { useAppDispatch, useAppSelector } from 'hooks';
 
 const NaturalProofsEditorTable = () => {
-  return <div className='natural-proofs-editor__table'>Table</div>;
+  const dispatch = useAppDispatch();
+  const tableData = useAppSelector(propositionsSelectors.getDirectProofsTableData);
+  const language = useAppSelector(settingsSelectors.getLanguage);
+  const [selectedIds, setSelectedIds] = useState<string[]>(useAppSelector(propositionsSelectors.getSelectedIds));
+  const takeSelectedIds = (ids: string[]) => setSelectedIds(ids);
+
+  useEffect(() => {
+    dispatch(actions.setSelectedIds(selectedIds));
+  }, [selectedIds]);
+
+  const noStepsFiller = <div className='direct-proofs-editor__filler'>{fillerText[language]}</div>;
+
+  return (
+    <div className='natural-proofs-editor__table'>
+      <Table columns={constants.directProofsEditorTableColumns} data={tableData} hasCheckboxColumn passSelectedIds={takeSelectedIds} />
+      {tableData.length === 0 && noStepsFiller}
+    </div>
+  );
 };
 
 export default NaturalProofsEditorTable;
