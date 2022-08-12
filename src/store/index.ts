5a2fcd7af79b1fe264fa637ecb45bae62c1394c9
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-08-12 14:13:49 +0200

    ts-70: add propositionsNaturalProofsSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { propositionsDirectProofsSlice } from './propositions/direct-proofs/slice';
+import { propositionsNaturalProofsSlice } from './propositions/natural-proofs/slice';
 import { settingsSlice } from './settings/slice';
 
 export const store = configureStore({
   reducer: {
     settings: settingsSlice.reducer,
     propositionsDirectProofs: propositionsDirectProofsSlice.reducer,
+    propositionsNaturalProofs: propositionsNaturalProofsSlice.reducer,
   },
 });
