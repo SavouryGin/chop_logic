5a03f14157738247d1fca26a2d50423e59144a29
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-08-14 11:27:08 +0200

    ts-71: add Premise form

--- src/components/propositions/toolbars/natural-proofs/index.tsx
+++ src/components/propositions/toolbars/natural-proofs/index.tsx
@@ -1,12 +1,20 @@
 import Button from 'components/controls/button';
 import React from 'react';
 import { ButtonID } from 'enums';
+import { propositionsNaturalProofsActions as actions } from 'store/propositions/natural-proofs/slice';
 import { soundPlayer } from 'helpers/sounds';
+import { useAppDispatch } from 'hooks';
 
 const NaturalProofsEditorToolbar = () => {
+  const dispatch = useAppDispatch();
+
+  const openPremise = () => {
+    dispatch(actions.setUpFlag({ flag: 'isPremiseOpened', value: true }));
+  };
+
   return (
     <div className='natural-proofs-editor__toolbar'>
-      <Button buttonId={ButtonID.Premise} sound={soundPlayer.keyboard} size='large' />
+      <Button buttonId={ButtonID.Premise} sound={soundPlayer.keyboard} size='large' onClick={openPremise} />
       <Button buttonId={ButtonID.Reiteration} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.NotIntroduction} sound={soundPlayer.keyboard} size='large' />
       <Button buttonId={ButtonID.AndIntroduction} sound={soundPlayer.keyboard} size='large' />
