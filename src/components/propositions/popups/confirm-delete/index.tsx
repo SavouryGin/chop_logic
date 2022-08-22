10f289d63dcd38db646ced879d201890a3941f43
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-08-22 14:38:03 +0200

    bug-9: add content to the confirmation popup

--- src/components/propositions/popups/confirm-delete/index.tsx
+++ src/components/propositions/popups/confirm-delete/index.tsx
@@ -1,7 +1,30 @@
+import Button from 'components/controls/button';
 import React from 'react';
+import Table from 'components/table';
+import constants from 'presets/propositions';
+import { ButtonID } from 'enums';
+import { popupsTexts } from 'texts';
+import { propositionsDPSelectors as selectors } from 'store/propositions/direct-proofs/selectors';
+import { settingsSelectors } from 'store/settings/selectors';
+import { useAppSelector } from 'hooks';
+import './styles.scss';
 
 const ConfirmDeleteProofStepsPopup = () => {
-  return <div>Hello</div>;
+  const dependencies = useAppSelector(selectors.getDependentItems);
+  const language = useAppSelector(settingsSelectors.getLanguage);
+
+  return (
+    <div className='confirm-delete-formulas'>
+      <div className='confirm-delete-formulas__text'>{popupsTexts.deleteConfirmation[language]}</div>
+      <Table
+        className='confirm-delete-formulas__table'
+        columns={constants.directProofsEditorTableColumns}
+        data={dependencies}
+        hasCheckboxColumn={false}
+      />
+      <Button buttonId={ButtonID.Apply} size='large' />
+    </div>
+  );
 };
 
 export default ConfirmDeleteProofStepsPopup;
