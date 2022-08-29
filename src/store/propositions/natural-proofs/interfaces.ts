f17f1a02d61cc83c8d20316703d1edca48b550e9
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-08-29 21:04:39 +0200

    bug-10: update natural proofs slice

--- src/store/propositions/natural-proofs/interfaces.ts
+++ src/store/propositions/natural-proofs/interfaces.ts
@@ -4,12 +4,15 @@ export interface PropositionsNaturalProofsInitialState {
   flags: PropositionsNaturalProofsFlags;
   tableData: NaturalProofsTableDataItem[];
   selectedIds: string[];
+  dependentItems: NaturalProofsTableDataItem[];
+  error: string | null;
 }
 
 export interface PropositionsNaturalProofsFlags {
   isPremiseOpened: boolean;
   isReplacerFormOpened: boolean;
   isAssumptionOpened: boolean;
+  isConfirmDeletePopupOpened: boolean;
 }
 
 export type PropositionsNaturalProofsFlag = keyof PropositionsNaturalProofsFlags;
