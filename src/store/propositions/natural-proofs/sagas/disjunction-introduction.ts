63ddc85795d145687b7ff8f57940c5cfef99ee66
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-09-08 10:09:17 +0200

    ts-72: implement DI saga

--- src/store/propositions/natural-proofs/sagas/disjunction-introduction.ts
+++ src/store/propositions/natural-proofs/sagas/disjunction-introduction.ts
@@ -1,3 +1,4 @@
+import executor from 'logic/propositions/executor';
 import { NaturalProofsTableItem } from '../interfaces';
 import { SagaIterator } from 'redux-saga';
 import { propositionsNPActions as actions } from 'store/propositions/natural-proofs/slice';
@@ -11,9 +12,16 @@ export function* disjunctionIntroductionWatcher(): Generator {
 export function* disjunctionIntroductionSaga(action: { payload: string }): SagaIterator {
   try {
     const selectedItems: NaturalProofsTableItem[] = yield select(selectors.getSelectedTableItems);
+    const tableData: NaturalProofsTableItem[] = yield select(selectors.getTableData);
+    const dataLength: number = yield select(selectors.getTableDataLength);
+    const level: number = yield select(selectors.getLastTableItemLevel);
+    const rawInput = action.payload;
 
-    const input = action.payload;
-    console.log('SAGA', selectedItems, input);
+    const newItems = executor.performDI({ rawInput, level, dataLength, selectedItems });
+
+    console.log('SAGA', selectedItems, rawInput, dataLength);
+    yield put(actions.setTableData([...tableData, ...newItems]));
+    yield put(actions.setSelectedIds([]));
   } catch (error: unknown) {
     const errorMessage = (error as any)?.message || 'Delete action error';
     yield put(actions.setError(errorMessage));
