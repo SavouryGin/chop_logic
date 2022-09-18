c07962b72e4ebfbe52f455ee334d5afbb2576e6c
Dmitrii <Dmitrii.Suroviagin@gmail.com>
2022-09-18 09:50:03 +0200

    ts-74: fix performCI() logic

--- src/store/propositions/natural-proofs/sagas/index.ts
+++ src/store/propositions/natural-proofs/sagas/index.ts
@@ -1,10 +1,16 @@
 import { all } from 'redux-saga/effects';
+import { conjunctionIntroductionWatcher } from './conjunction-introduction';
 import { deleteNaturalProofStepsWatcher } from './delete-steps';
 import { disjunctionEliminationWatcher } from './disjunction-elimination';
 import { disjunctionIntroductionWatcher } from './disjunction-introduction';
 
 function* propositionsNPSagas(): Generator {
-  yield all([deleteNaturalProofStepsWatcher(), disjunctionIntroductionWatcher(), disjunctionEliminationWatcher()]);
+  yield all([
+    deleteNaturalProofStepsWatcher(),
+    disjunctionIntroductionWatcher(),
+    disjunctionEliminationWatcher(),
+    conjunctionIntroductionWatcher(),
+  ]);
 }
 
 export default propositionsNPSagas;
