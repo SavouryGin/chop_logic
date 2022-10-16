0c4c76025494b43609fdd7e6ea3a5a0dd0dc27a3
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-10-16 15:41:18 +0200

    ts-78: add a new NP definitions component

---
+++ src/pages/propositions/tabs/natural-proofs/definitions/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.scss';
+
+const NaturalProofsPropositionDefinitions = () => {
+  return <div>Definitions</div>;
+};
+
+export default NaturalProofsPropositionDefinitions;
