fad84d5cf45c74557b1fb3ee31a1bed64a556b9f
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-10-16 15:41:18 +0200

    ts-78: add a new NP definitions component

--- src/presets/propositions.tsx
+++ src/presets/propositions.tsx
@@ -3,6 +3,7 @@ import DirectProofsEditor from 'pages/propositions/tabs/direct-proofs/editor';
 import DirectProofsPropositionDefinitions from 'pages/propositions/tabs/direct-proofs/definitions';
 import HeuristicsOfPropositions from 'pages/propositions/tabs/natural-proofs/heuristics';
 import NaturalProofsEditor from 'pages/propositions/tabs/natural-proofs/editor';
+import NaturalProofsPropositionDefinitions from 'pages/propositions/tabs/natural-proofs/definitions';
 import React from 'react';
 import RulesOfInference from 'pages/propositions/tabs/natural-proofs/rules';
 import { GreekSymbol, LogicalSymbolHexCode, LogicalSymbolRawInput } from 'enums';
@@ -32,6 +33,11 @@ export const propositionsNaturalProofsTabs: TabItem[] = [
     tabContent: <NaturalProofsEditor />,
     tabTitle: titles.natural,
     tabId: 'natural-proofs-editor',
+  },
+  {
+    tabContent: <NaturalProofsPropositionDefinitions />,
+    tabTitle: titles.definitions,
+    tabId: 'natural-proofs-definitions',
   },
   {
     tabContent: <HeuristicsOfPropositions />,
