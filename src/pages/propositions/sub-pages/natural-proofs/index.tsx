5e9e79ab49b3651dbc736b281e86bcca2cce8eae
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-10-17 07:47:05 +0200

    ts-78: add new pages to the app router

--- src/pages/propositions/sub-pages/natural-proofs/index.tsx
+++ src/pages/propositions/sub-pages/natural-proofs/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import TabList from 'components/tab-list';
 import formatClassName from 'helpers/formatters/format-class-name';
 import { Icon } from 'enums';
-import { propositionsDirectProofsTabs } from 'pages/propositions/constants';
+import { propositionsNaturalProofsTabs } from 'pages/propositions/constants';
 import { settingsSelectors } from 'store/settings/selectors';
 import { titles } from 'texts/propositions';
 import { useAppSelector } from 'hooks';
@@ -14,7 +14,7 @@ const PropositionsNaturalProofs = () => {
   return (
     <article className='propositions-natural-proofs'>
       <h2 className={formatClassName(['propositions-natural-proofs__title', Icon.Propositions])}>{titles.page[language]}</h2>
-      <TabList tabs={propositionsDirectProofsTabs} className='propositions-natural-proofs__tabs' />
+      <TabList tabs={propositionsNaturalProofsTabs} className='propositions-natural-proofs__tabs' />
     </article>
   );
 };
