bd2dae7c9f57e2cb1a6eba486d7ad43fe1a1b5d8
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-10-21 08:19:19 +0200

    ts-78: add groupRoutesMapItemsByParentPage()

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,8 +1,7 @@
-import AppLink from 'components/app-link';
 import React from 'react';
 import formatClassName from 'helpers/formatters/format-class-name';
 import { CommonProps } from 'types';
-import { groupArrayOfObjectsBy } from 'helpers/formatters/group-array-of-objects-by';
+import { getNavigationLinksList } from './helpers';
 import { routesMap } from 'router/map';
 import { settingsSelectors } from 'store/settings/selectors';
 import { uiElementTexts } from 'texts';
@@ -14,23 +13,10 @@ const Navigation = (props: CommonProps) => {
   const language = useAppSelector(settingsSelectors.getLanguage);
   const navigationClassNames = formatClassName(['navigation', props.className, { navigation_closing: isClosingAnimationActive }]);
 
-  const topLinksMap = routesMap.filter((item) => !item.parentPageId);
-
-  const nestedLinks = groupArrayOfObjectsBy(routesMap, 'parentPageId');
-  console.log(nestedLinks);
-
-  const topLinks = topLinksMap.map((item) => {
-    return (
-      <li key={item.key}>
-        <AppLink path={item.url} text={item.title[language]} isNavigation icon={item.icon} />
-      </li>
-    );
-  });
-
   return (
     <nav className={navigationClassNames}>
       <h2 className='navigation__header'>{uiElementTexts.navHeader[language]}</h2>
-      <ul className='navigation__list'>{topLinks}</ul>
+      <ul className='navigation__list'>{getNavigationLinksList(routesMap, language)}</ul>
     </nav>
   );
 };
