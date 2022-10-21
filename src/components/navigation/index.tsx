0341c23ef9ea5ccda832b446ad1624d973496bad
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-10-21 07:45:23 +0200

    ts-78: add groupArrayOfObjectsBy() helper

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,6 +2,7 @@ import AppLink from 'components/app-link';
 import React from 'react';
 import formatClassName from 'helpers/formatters/format-class-name';
 import { CommonProps } from 'types';
+import { groupArrayOfObjectsBy } from 'helpers/formatters/group-array-of-objects-by';
 import { routesMap } from 'router/map';
 import { settingsSelectors } from 'store/settings/selectors';
 import { uiElementTexts } from 'texts';
@@ -14,6 +15,9 @@ const Navigation = (props: CommonProps) => {
   const navigationClassNames = formatClassName(['navigation', props.className, { navigation_closing: isClosingAnimationActive }]);
 
   const topLinksMap = routesMap.filter((item) => !item.parentPageId);
+
+  const nestedLinks = groupArrayOfObjectsBy(routesMap, 'parentPageId');
+  console.log(nestedLinks);
 
   const topLinks = topLinksMap.map((item) => {
     return (
