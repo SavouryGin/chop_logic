1ecff4f34464f4a5c89895f285ae6277bfc8e4db
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-10-27 08:12:44 +0200

    bug-12: install FocusLock library

--- src/components/modal-window/elements/modal-layout.tsx
+++ src/components/modal-window/elements/modal-layout.tsx
@@ -1,4 +1,5 @@
 import Button from 'components/controls/button';
+import FocusLock from 'react-focus-lock';
 import React from 'react';
 import { ButtonID, Icon } from 'enums';
 import { soundPlayer } from 'helpers/sounds';
@@ -14,15 +15,17 @@ type ModalLayoutProps = {
 
 const ModalLayout = ({ windowClassName, contentClassName, title, id, onClose, content }: ModalLayoutProps) => {
   return (
-    <div className={windowClassName} role='dialog' aria-modal='true' id={id}>
-      <header className='modal-window__header' id='modal-window-heading'>
-        {title}
-        <Button buttonId={ButtonID.Cancel} onClick={onClose} icon={Icon.Cancel} sound={soundPlayer.slideClick} size='small' />
-      </header>
-      <div className={contentClassName} role='region' aria-labelledby='modal-window-heading'>
-        {content}
+    <FocusLock>
+      <div className={windowClassName} role='dialog' aria-modal='true' id={id}>
+        <header className='modal-window__header' id='modal-window-heading'>
+          {title}
+          <Button buttonId={ButtonID.Cancel} onClick={onClose} icon={Icon.Cancel} sound={soundPlayer.slideClick} size='small' />
+        </header>
+        <div className={contentClassName} role='region' aria-labelledby='modal-window-heading'>
+          {content}
+        </div>
       </div>
-    </div>
+    </FocusLock>
   );
 };
 
