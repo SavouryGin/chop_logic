21f415ece0bdb72ffe8683901e3e7c7387d3989d
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-11-03 08:37:40 +0100

    ts-82: update navigation tests

--- src/components/navigation/__tests__/navigation.test.tsx
+++ src/components/navigation/__tests__/navigation.test.tsx
@@ -1,13 +1,13 @@
 import Navigation from '../index';
 import React from 'react';
 import renderWithRedux from 'helpers/test-utils/render-with-redux';
+import { Page } from 'enums';
 import { combineReducers } from '@reduxjs/toolkit';
+import { fireEvent, screen } from '@testing-library/react';
 import { routesMap } from 'router/map';
-import { screen } from '@testing-library/react';
 import { settingsInitialState, settingsSlice } from 'store/settings/slice';
 
 describe('Navigation component:', () => {
-  const pagesCount = routesMap.length;
   const testProps = {
     className: 'test-class-name',
     isOpened: true,
@@ -43,24 +43,41 @@ describe('Navigation component:', () => {
     expect(list).toHaveClass('navigation__list');
   });
 
-  it('the amount of list items equals the number of pages', () => {
+  it('the amount of list items equals the number of top pages', () => {
+    const pagesCount = routesMap.filter((item) => item.parentPageId === Page.Home).length;
     expect(screen.getAllByRole('listitem')).toHaveLength(pagesCount);
   });
 
-  it('each list item displays the page title', () => {
-    const listItems = screen.getAllByRole('listitem');
+  it('the list contains buttons to open nested links', async () => {
+    const pagesCount = routesMap.length;
+    const buttons = screen.getAllByRole('button');
+    buttons.forEach((button) => {
+      fireEvent.click(button);
+    });
+
+    const links = await screen.findAllByRole('link');
+    expect(links).toHaveLength(pagesCount);
+  });
+
+  it('each list item displays the page title', async () => {
+    const buttons = screen.getAllByRole('button');
+    buttons.forEach((button) => {
+      fireEvent.click(button);
+    });
+
+    const listItems = await screen.findAllByRole('listitem');
     for (let i = 0; i < listItems.length; i++) {
       expect(listItems[i]).toHaveTextContent(routesMap[i].title.en);
     }
   });
 
-  it('the amount of links equals the number of pages', () => {
-    const links = screen.getAllByRole('link');
-    expect(links).toHaveLength(pagesCount);
-  });
+  it('each link has the page icon', async () => {
+    const buttons = screen.getAllByRole('button');
+    buttons.forEach((button) => {
+      fireEvent.click(button);
+    });
 
-  it('each link has the page icon', () => {
-    const links = screen.getAllByRole('link');
+    const links = await screen.findAllByRole('link');
     for (let i = 0; i < links.length; i++) {
       expect(links[i]).toHaveClass(routesMap[i].icon);
     }
