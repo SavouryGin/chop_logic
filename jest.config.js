f157f32710e593a323577b11abd9f7d1c2a7d572
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-11-05 16:19:25 +0100

    ts-82: add tests for the App component

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,7 @@ module.exports = {
     '^texts(.*)$': '<rootDir>/src/texts$1',
     '^presets(.*)$': '<rootDir>/src/presets$1',
     '^router(.*)$': '<rootDir>/src/router$1',
+    '^app(.*)$': '<rootDir>/src/app$1',
     '^__mocks__(.*)$': '<rootDir>/src/__mocks__$1',
     '\\.(css|scss)$': '<rootDir>/src/__mocks__/style-mock.js',
   },
