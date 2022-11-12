ca3eb235a314301b803909509f16faccf1b7809f
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-11-12 09:50:29 +0100

    ts-82: add conjunction sagas tests

---
+++ src/store/propositions/natural-proofs/__tests__/conjunction-elimination-saga.test.ts
@@ -0,0 +1,30 @@
+import mocks from '__mocks__/data/propositions/table-items';
+import { propositionsNPActions as actions } from 'store/propositions/natural-proofs/slice';
+import { conjunctionEliminationSaga, conjunctionEliminationWatcher } from '../sagas/conjunction-elimination';
+import { propositionsNPSelectors as selectors } from 'store/propositions/natural-proofs/selectors';
+import { testSaga } from 'redux-saga-test-plan';
+
+describe('NP conjunctionEliminationSaga tests', () => {
+  it('conjunctionEliminationWatcher() should call the saga on action', () => {
+    testSaga(conjunctionEliminationWatcher).next().takeEvery(actions.eliminateConjunction, conjunctionEliminationSaga).next().isDone();
+  });
+
+  it('conjunctionEliminationSaga() should catch the error', () => {
+    testSaga(conjunctionEliminationSaga)
+      .next()
+      .select(selectors.getSelectedTableItems)
+      .next([])
+      .select(selectors.getTableData)
+      .next([])
+      .select(selectors.getTableDataLength)
+      .next(0)
+      .select(selectors.getLastTableItemLevel)
+      .next(0)
+      .select(selectors.getLastItemAssumptionId)
+      .next(null)
+      .throw(mocks.error)
+      .put(actions.setError(mocks.error.message))
+      .next()
+      .isDone();
+  });
+});
