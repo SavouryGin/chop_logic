52779f1aad9907cd098f12d7cf401409ebeae15e
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-11-13 11:41:46 +0100

    ts-82: add implication sagas tests

---
+++ src/store/propositions/natural-proofs/__tests__/implication-elimination-saga.test.ts
@@ -0,0 +1,28 @@
+import mocks from '__mocks__/data/propositions/table-items';
+import { propositionsNPActions as actions } from 'store/propositions/natural-proofs/slice';
+import { implicationEliminationSaga, implicationEliminationWatcher } from '../sagas/implication-elimination';
+import { propositionsNPSelectors as selectors } from 'store/propositions/natural-proofs/selectors';
+import { testSaga } from 'redux-saga-test-plan';
+
+describe('NP implicationEliminationSaga tests', () => {
+  it('implicationEliminationWatcher() should call the saga on action', () => {
+    testSaga(implicationEliminationWatcher).next().takeEvery(actions.eliminateImplication, implicationEliminationSaga).next().isDone();
+  });
+
+  it('implicationEliminationSaga() should catch the error', () => {
+    testSaga(implicationEliminationSaga)
+      .next()
+      .select(selectors.getSelectedTableItems)
+      .next([])
+      .select(selectors.getTableData)
+      .next([])
+      .select(selectors.getTableDataLength)
+      .next(0)
+      .select(selectors.getLastTableItemLevel)
+      .next(0)
+      .throw(mocks.error)
+      .put(actions.setError(mocks.error.message))
+      .next()
+      .isDone();
+  });
+});
