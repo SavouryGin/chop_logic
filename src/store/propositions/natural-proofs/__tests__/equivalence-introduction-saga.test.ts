8736bc7660842d9000c6fdfa8ed34f3c9b31ab02
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-11-13 11:34:25 +0100

    ts-82: add equivalence sagas tests

---
+++ src/store/propositions/natural-proofs/__tests__/equivalence-introduction-saga.test.ts
@@ -0,0 +1,28 @@
+import mocks from '__mocks__/data/propositions/table-items';
+import { propositionsNPActions as actions } from 'store/propositions/natural-proofs/slice';
+import { equivalenceIntroductionSaga, equivalenceIntroductionWatcher } from '../sagas/equivalence-introduction';
+import { propositionsNPSelectors as selectors } from 'store/propositions/natural-proofs/selectors';
+import { testSaga } from 'redux-saga-test-plan';
+
+describe('NP equivalenceIntroductionSaga tests', () => {
+  it('equivalenceIntroductionWatcher() should call the saga on action', () => {
+    testSaga(equivalenceIntroductionWatcher).next().takeEvery(actions.createEquivalence, equivalenceIntroductionSaga).next().isDone();
+  });
+
+  it('equivalenceIntroductionSaga() should catch the error', () => {
+    testSaga(equivalenceIntroductionSaga)
+      .next()
+      .select(selectors.getSelectedTableItems)
+      .next([])
+      .select(selectors.getTableData)
+      .next([])
+      .select(selectors.getTableDataLength)
+      .next(0)
+      .select(selectors.getLastTableItemLevel)
+      .next(0)
+      .throw(mocks.error)
+      .put(actions.setError(mocks.error.message))
+      .next()
+      .isDone();
+  });
+});
