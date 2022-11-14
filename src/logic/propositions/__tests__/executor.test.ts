d471c31b1621d2ce59b818eb41bfc6abf74b7941
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-11-14 10:19:26 +0100

    ts-82: add tests for performCI() & performIEforNP

--- src/logic/propositions/__tests__/executor.test.ts
+++ src/logic/propositions/__tests__/executor.test.ts
@@ -1,5 +1,6 @@
 import executor from '../executor';
 import mocks from '__mocks__/data/propositions/formulas-items';
+import tMocks from '__mocks__/data/propositions/table-items';
 import { PropositionalError } from 'errors/propositional-error';
 
 describe('Propositions executor tests:', () => {
@@ -20,5 +21,25 @@ describe('Propositions executor tests:', () => {
     expect(() => {
       executor.performIE(mocks.propositionalFormula, mocks.secondSubFormula);
     }).toThrow(PropositionalError);
+  });
+
+  it('performCI() creates correct conjunctions', () => {
+    const data = {
+      level: 0,
+      dataLength: 1,
+      selectedItems: [tMocks.npTableCIandCE[0]],
+      assumptionId: null,
+    };
+    expect(executor.performCI({ ...data })).toEqual([{ ...tMocks.npTableCIandCE[1], id: expect.any(String) }]);
+  });
+
+  it('performIEforNP() creates a correct modus ponens', () => {
+    const data = {
+      level: 1,
+      dataLength: 2,
+      selectedItems: [tMocks.npTableDataIEandII[0], tMocks.npTableDataIEandII[1]],
+      assumptionId: tMocks.npTableDataIEandII[1].assumptionId,
+    };
+    expect(executor.performIEforNP({ ...data })).toEqual({ ...tMocks.npTableDataIEandII[2], id: expect.any(String) });
   });
 });
