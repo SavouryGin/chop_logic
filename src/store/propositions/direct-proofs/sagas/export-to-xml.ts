4b10c26e53dce137a0eac1cdea3b0b25333f4458
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-11-24 09:07:15 +0100

    ts-83: add draft exportDPToXMLSaga()

---
+++ src/store/propositions/direct-proofs/sagas/export-to-xml.ts
@@ -0,0 +1,16 @@
+import { SagaIterator } from 'redux-saga';
+import { propositionsDPActions as actions } from 'store/propositions/direct-proofs/slice';
+import { put, takeEvery } from 'redux-saga/effects';
+
+export function* exportDPToXMLWatcher(): Generator {
+  yield takeEvery(actions.exportToXML, exportDPToXMLSaga);
+}
+
+export function* exportDPToXMLSaga(): SagaIterator {
+  try {
+    console.log('Export Saga');
+  } catch (error: unknown) {
+    const errorMessage = (error as any)?.message || 'Export to XML error';
+    yield put(actions.setError(errorMessage));
+  }
+}
--- src/store/propositions/direct-proofs/sagas/index.ts
+++ src/store/propositions/direct-proofs/sagas/index.ts
@@ -1,8 +1,9 @@
 import { all } from 'redux-saga/effects';
 import { deleteDirectProofStepsWatcher } from './delete-steps';
+import { exportDPToXMLWatcher } from './export-to-xml';
 
 function* propositionsDPSagas(): Generator {
-  yield all([deleteDirectProofStepsWatcher()]);
+  yield all([deleteDirectProofStepsWatcher(), exportDPToXMLWatcher()]);
 }
 
 export default propositionsDPSagas;
