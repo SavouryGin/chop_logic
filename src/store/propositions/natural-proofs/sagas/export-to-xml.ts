81c0d08400b4fe604742d693e7ca710ad08532bb
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-11-27 16:21:54 +0100

    ts-83: implement exportNPToXMLSaga()

--- src/store/propositions/natural-proofs/sagas/export-to-xml.ts
+++ src/store/propositions/natural-proofs/sagas/export-to-xml.ts
@@ -1,16 +1,24 @@
 import { SagaIterator } from 'redux-saga';
 import { propositionsNPActions as actions } from 'store/propositions/natural-proofs/slice';
+import { createAndSaveXMLFile } from 'helpers/files/create-and-save-xml-file';
 import { put, takeEvery } from 'redux-saga/effects';
 
 export function* exportNPToXMLWatcher(): Generator {
   yield takeEvery(actions.exportToXML, exportNPToXMLSaga);
 }
 
-export function* exportNPToXMLSaga(): SagaIterator {
+export function* exportNPToXMLSaga(action: { payload: string | undefined }): SagaIterator {
   try {
-    console.log('Export Saga');
+    const fileName = action.payload;
 
-    yield put(actions.setUpFlag({ flag: 'isNameInputPopupVisible', value: true }));
+    if (!fileName) {
+      yield put(actions.setUpFlag({ flag: 'isNameInputPopupVisible', value: true }));
+
+      return;
+    }
+
+    const mockData = `<note>Hello</note>`;
+    createAndSaveXMLFile(mockData, fileName);
   } catch (error: unknown) {
     const errorMessage = (error as any)?.message || 'Export to XML error';
     yield put(actions.setError(errorMessage));
