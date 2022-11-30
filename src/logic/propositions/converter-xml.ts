00ce184b0c26820e3262b09d8b4b3fd04d7fca08
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-11-30 08:59:01 +0100

    ts-84: add convertItemsArrayToXML() method

--- src/logic/propositions/converter-xml.ts
+++ src/logic/propositions/converter-xml.ts
@@ -3,9 +3,14 @@ import { DirectProofsTableItem } from 'store/propositions/direct-proofs/interfac
 const converterXML = {
   convertDPtoXML(tableData: DirectProofsTableItem[]): string {
     console.log(tableData);
-    const mockData = `<note>Hello</note>`;
 
-    return mockData;
+    return `<propositionsDirectProof>\n${this.convertItemsArrayToXML(tableData)}\n</propositionsDirectProof>`;
+  },
+
+  convertItemsArrayToXML(data: DirectProofsTableItem[]): string {
+    const itemsArray = data.map((item) => `    <tableItem>\n${JSON.stringify(item)}\n    </tableItem>`);
+
+    return itemsArray.join('\n');
   },
 };
 
