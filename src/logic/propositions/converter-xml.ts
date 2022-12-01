57eaf0b201bea088a373a04a8f16b4418d06f27f
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-12-01 09:41:24 +0100

    ts-84: add draft dpItemToXML method()

--- src/logic/propositions/converter-xml.ts
+++ src/logic/propositions/converter-xml.ts
@@ -1,16 +1,44 @@
 import { DirectProofsTableItem } from 'store/propositions/direct-proofs/interfaces';
 
 const converterXML = {
-  convertDPtoXML(tableData: DirectProofsTableItem[]): string {
+  dpToXML(tableData: DirectProofsTableItem[]): string {
     console.log(tableData);
 
-    return `<propositionsDirectProof>\n${this.convertItemsArrayToXML(tableData)}\n</propositionsDirectProof>`;
+    return `<propositionsDirectProof>\n${this.dpArrayToXML(tableData)}\n</propositionsDirectProof>`;
   },
 
-  convertItemsArrayToXML(data: DirectProofsTableItem[]): string {
-    const itemsArray = data.map((item) => `    <tableItem>\n${JSON.stringify(item)}\n    </tableItem>`);
+  dpArrayToXML(data: DirectProofsTableItem[]): string {
+    const itemsArray = data.map((item) => this.dpItemToXML(item));
 
     return itemsArray.join('\n');
+  },
+
+  dpItemToXML(item: DirectProofsTableItem): string {
+    const expression = JSON.stringify(item.expression);
+    const friendlyExpression = JSON.stringify(item.friendlyExpression);
+    const formula = JSON.stringify(item.formula);
+    const id = JSON.stringify(item.id);
+    const step = JSON.stringify(item.step);
+    const rawInput = JSON.stringify(item.rawInput);
+    const comment = JSON.stringify(item.comment);
+    const dependentOn = JSON.stringify(item.dependentOn);
+
+    const xml = `
+    <tableItem>
+    ${id}
+    ${step}
+    ${rawInput}
+    ${comment}
+    ${dependentOn}
+    ${formula}
+    ${expression}
+    ${friendlyExpression}
+    </tableItem>
+    `;
+
+    console.log(xml);
+
+    return 'mockItem';
   },
 };
 
