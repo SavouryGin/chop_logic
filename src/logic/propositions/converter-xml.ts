207edbd2e47f4a6b1acad105fa3a0080dbcda6c3
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-12-05 09:20:15 +0100

    ts-84: add symbolToXML() method

--- src/logic/propositions/converter-xml.ts
+++ src/logic/propositions/converter-xml.ts
@@ -1,10 +1,10 @@
 import { DirectProofsTableItem } from 'store/propositions/direct-proofs/interfaces';
-import { Language, LocalText, PropositionalFormula } from 'types';
+import { Language, LocalText, PropositionalExpression, PropositionalFormula, PropositionalSymbol } from 'types';
 import { PropositionalOperator } from 'enums';
 
 const converterXML = {
   dpToXML(tableData: DirectProofsTableItem[]): string {
-    return `<propositionsDirectProof>${this.dpArrayToXML(tableData)}\n</propositionsDirectProof>`;
+    return `<propositionsDirectProof>${this.dpArrayToXML(tableData)}</propositionsDirectProof>`;
   },
 
   dpArrayToXML(data: DirectProofsTableItem[]): string {
@@ -17,17 +17,9 @@ const converterXML = {
     const expression = JSON.stringify(item.expression);
     const friendlyExpression = JSON.stringify(item.friendlyExpression);
 
-    const xml = `
-  <tableItem>
-    ${this.idToXML(item.id)}
-    ${this.stepToXML(item.step)}
-    ${this.rawInputToXML(item.rawInput)}
-    ${this.commentToXML(item.comment)}
-    ${this.dependentOnToXML(item.dependentOn)}
-    ${this.formulaToXML(item.formula)}
-    ${expression}
-    ${friendlyExpression}
-  </tableItem>`;
+    const xml = `<tableItem>${this.idToXML(item.id)}${this.stepToXML(item.step)}${this.rawInputToXML(item.rawInput)}${this.commentToXML(
+      item.comment,
+    )}${this.dependentOnToXML(item.dependentOn)}${this.formulaToXML(item.formula)}${expression}${friendlyExpression}</tableItem>`;
 
     console.log(xml);
 
@@ -73,24 +65,30 @@ const converterXML = {
 
   formulaToXML(formula: PropositionalFormula): string {
     if (Array.isArray(formula.values) && formula.operator !== PropositionalOperator.Var) {
-      const nestedFormulas = formula.values.map((value) => this.formulaToXML(value)).join('\n');
+      const nestedFormulas = formula.values.map((value) => this.formulaToXML(value)).join('');
 
-      return `<propositionalFormula>
-      ${this.operatorToXML(formula.operator)}
-      <values>
-        ${nestedFormulas}
-      </values>
-    </propositionalFormula>`;
+      return `<propositionalFormula>${this.operatorToXML(formula.operator)}<values>${nestedFormulas}</values></propositionalFormula>`;
     } else {
-      return `<propositionalFormula>
-      ${this.operatorToXML(formula.operator)}
-      <values>${formula.values.toString()}</values>
-    </propositionalFormula>`;
+      return `<propositionalFormula>${this.operatorToXML(
+        formula.operator,
+      )}<values>${formula.values.toString()}</values></propositionalFormula>`;
     }
   },
 
   operatorToXML(operator: PropositionalOperator): string {
     return `<operator>${operator.toString()}</operator>`;
+  },
+
+  expressionToXML(expression: PropositionalExpression): string {
+    console.log(expression);
+
+    return `<propositionalExpression></propositionalExpression>`;
+  },
+
+  symbolToXML(symbol: PropositionalSymbol): string {
+    return `<propositionalSymbol><input>${symbol.input}</input><type>${symbol.type}</type><position>${
+      symbol.position
+    }</position><representation>${symbol.representation || ''}</representation></propositionalSymbol>`;
   },
 };
 
