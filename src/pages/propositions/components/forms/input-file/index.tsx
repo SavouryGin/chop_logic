0f66a99aa2e62c2eb7bde0eec51d099d92770a5d
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-12-11 16:09:46 +0100

    ts-86: add handlers for InputFileForm

--- src/pages/propositions/components/forms/input-file/index.tsx
+++ src/pages/propositions/components/forms/input-file/index.tsx
@@ -1,47 +1,32 @@
+import FileInput from 'components/controls/file-input';
 import Form from 'components/controls/form';
-import React, { memo, useMemo, useState } from 'react';
-import TextInput from 'components/controls/text-input';
-import regularExpressions from 'helpers/regular-expressions';
+import React, { memo, useState } from 'react';
 import { ButtonID, InputID } from 'enums';
 import { FormValues } from 'types';
-import { propositionsDPActions } from 'store/propositions/direct-proofs/slice';
-import { propositionsNPActions } from 'store/propositions/natural-proofs/slice';
-import { useAppDispatch } from 'hooks';
+// import { useAppDispatch } from 'hooks';
 import './styles.scss';
 
 const InputFileForm = ({ mode }: { mode: 'natural' | 'direct' }) => {
-  const dispatch = useAppDispatch();
-  const initialName = mode === 'direct' ? 'MyDirectProof' : 'MyNaturalProof';
-  const fileNameInitialValue = { fileName: initialName };
-  const [formValue, setFormValue] = useState(fileNameInitialValue);
-
-  const isFormInvalid = useMemo(() => {
-    return !formValue.fileName.match(regularExpressions.fileName);
-  }, [formValue.fileName]);
+  // const dispatch = useAppDispatch();
+  const fileNameInitialValue = { xmlFile: null };
+  // const [formValue, setFormValue] = useState(fileNameInitialValue);
+  const [userFile, setUserFile] = useState<{ xmlFile: File | null }>(fileNameInitialValue);
 
   const formContent = (
-    <TextInput
-      name='fileName'
-      inputId={InputID.FileName}
-      className='file-name-form__input'
-      isRequired
-      maxLength={100}
-      defaultValue={initialName}
-    />
+    <FileInput name='xmlFile' inputId={InputID.FileInput} label='XML File:' className='file-name-form__input' isRequired />
   );
 
-  const takeValues = (values: FormValues) => setFormValue(values as typeof fileNameInitialValue);
+  const takeValues = (value: FormValues) => setUserFile(value as typeof fileNameInitialValue);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(userFile);
     if (mode === 'direct') {
-      dispatch(propositionsDPActions.setUpFlag({ flag: 'isNameInputPopupVisible', value: false }));
-      dispatch(propositionsDPActions.exportToXML(formValue.fileName));
+      console.log('Direct');
     }
 
     if (mode === 'natural') {
-      dispatch(propositionsNPActions.setUpFlag({ flag: 'isNameInputPopupVisible', value: false }));
-      dispatch(propositionsNPActions.exportToXML(formValue.fileName));
+      console.log('Natural');
     }
   };
 
@@ -53,7 +38,6 @@ const InputFileForm = ({ mode }: { mode: 'natural' | 'direct' }) => {
         inputs={formContent}
         submitButtonId={ButtonID.Apply}
         passValues={takeValues}
-        isSubmitDisabled={isFormInvalid}
       />
     </div>
   );
