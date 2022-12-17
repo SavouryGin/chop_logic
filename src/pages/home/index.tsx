e922f817bd73871baf7af8c33750e5f9101cf9b1
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-12-17 11:21:49 +0100

    ts-87: add styles for the orbital spinner

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Spinner from 'components/spinner';
 import './styles.scss';
 
 const Home = (): React.ReactElement => {
-  return <article className='home'></article>;
+  return (
+    <article className='home'>
+      <Spinner />
+    </article>
+  );
 };
 
 export default Home;
