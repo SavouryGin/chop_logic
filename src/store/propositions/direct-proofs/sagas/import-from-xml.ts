5a7646333d7b6f925f61fb09ef9bf67b3aed230c
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-12-19 15:20:55 +0100

    ts-88: add a draft converterJS

--- src/store/propositions/direct-proofs/sagas/import-from-xml.ts
+++ src/store/propositions/direct-proofs/sagas/import-from-xml.ts
@@ -1,3 +1,4 @@
+import converterJS from 'logic/propositions/converter-js';
 import { SagaIterator } from 'redux-saga';
 import { propositionsDPActions as actions } from 'store/propositions/direct-proofs/slice';
 import { call, put, takeEvery } from 'redux-saga/effects';
@@ -11,9 +12,11 @@ export function* importDPFromXMLSaga(action: { payload: { file: File } }): SagaI
   yield put(actions.setUpFlag({ flag: 'isLoading', value: true }));
   try {
     const { file } = action.payload;
-    const text = yield call(readUserTextFile, file);
+    const text: string = yield call(readUserTextFile, file);
+    const tableData = converterJS.xmlToDPTableData(text);
 
-    console.log('Text', text);
+    yield put(actions.setTableData(tableData));
+
     yield put(actions.setUpFlag({ flag: 'isUserFileFormVisible', value: false }));
   } catch (error: unknown) {
     const errorMessage = (error as any)?.message || 'Import from XML file error';
