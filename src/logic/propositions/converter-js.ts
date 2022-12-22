c398e8eb137a4cf4ecbd21961628b4e464c0c96d
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-12-22 17:16:33 +0100

    ts-88: add expression parsing methods

--- src/logic/propositions/converter-js.ts
+++ src/logic/propositions/converter-js.ts
@@ -25,6 +25,36 @@ const converterJS = {
 
     return value;
   },
+
+  getRawInput(input: string): string {
+    return input.replace(XMLTag.RInputOpen, '').replace(XMLTag.RInputClose, '');
+  },
+
+  getId(input: string): string {
+    return input.replace(XMLTag.IdOpen, '').replace(XMLTag.IdClose, '');
+  },
+
+  getExpressionInput(input: string): string {
+    return input.replace(XMLTag.InputOpen, '').replace(XMLTag.InputClose, '');
+  },
+
+  getExpressionRepresentation(input: string): string {
+    return input.replace(XMLTag.RepresentOpen, '').replace(XMLTag.RepresentClose, '');
+  },
+
+  getExpressionType(input: string): string {
+    return input.replace(XMLTag.TypeOpen, '').replace(XMLTag.TypeClose, '');
+  },
+
+  getExpressionPosition(input: string): number {
+    const value = +input.replace(XMLTag.PositionOpen, '').replace(XMLTag.PositionClose, '');
+
+    if (isNaN(value)) {
+      throw new PropositionalError('Cannot convert the step value from XML tag.', errorsTexts.semanticError);
+    }
+
+    return value;
+  },
 };
 
 export default Object.freeze(converterJS);
