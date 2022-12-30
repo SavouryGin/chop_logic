a5bd8537ef17e32db959437a4dfae2a7cf67b981
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2022-12-30 13:38:46 +0100

    ts-89: add exportDPToXMLSaga tests

---
+++ src/store/propositions/direct-proofs/__tests__/export-to-xml.test.ts
@@ -0,0 +1,44 @@
+import mocks from '__mocks__/data/propositions/table-items';
+import tMocks from '__mocks__/data/propositions/table-items';
+import { propositionsDPActions as actions } from 'store/propositions/direct-proofs/slice';
+import { createAndSaveXMLFile } from 'helpers/files/create-and-save-xml-file';
+import { exportDPToXMLSaga, exportDPToXMLWatcher } from '../sagas/export-to-xml';
+import { propositionsDPSelectors as selectors } from 'store/propositions/direct-proofs/selectors';
+import { testSaga } from 'redux-saga-test-plan';
+
+describe('DP exportDPToXMLSaga tests', () => {
+  const testFileName = 'test-name';
+
+  it('exportDPToXMLWatcher() should call the saga on action', () => {
+    testSaga(exportDPToXMLWatcher).next().takeEvery(actions.exportToXML, exportDPToXMLSaga).next().isDone();
+  });
+
+  it('exportDPToXMLSaga() should create an xml file', () => {
+    testSaga(exportDPToXMLSaga, { payload: testFileName })
+      .next()
+      .select(selectors.getTableData)
+      .next(tMocks.dpTableDataIE)
+      .call(createAndSaveXMLFile, tMocks.dpIEtoXML, testFileName)
+      .next()
+      .isDone();
+  });
+
+  it('exportDPToXMLSaga() should call the popup if fileName is not defined', () => {
+    testSaga(exportDPToXMLSaga, { payload: undefined })
+      .next()
+      .put(actions.setUpFlag({ flag: 'isNameInputPopupVisible', value: true }))
+      .next()
+      .isDone();
+  });
+
+  it('deleteDirectProofStepsSaga() should catch the error', () => {
+    testSaga(exportDPToXMLSaga, { payload: testFileName })
+      .next()
+      .select(selectors.getTableData)
+      .next(tMocks.dpTableDataIE)
+      .throw(mocks.error)
+      .put(actions.setError(mocks.error.message))
+      .next()
+      .isDone();
+  });
+});
