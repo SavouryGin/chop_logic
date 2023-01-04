5db1e4002a1d68fdad25bd03848f1a62ada93c94
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-01-04 09:21:48 +0100

    ts-90: modify setup-tests.ts

--- src/helpers/test-utils/setup-tests.ts
+++ src/helpers/test-utils/setup-tests.ts
@@ -1,1 +1,8 @@
+import crypto from 'crypto';
 import '@testing-library/jest-dom/extend-expect';
+
+Object.defineProperty(globalThis, 'crypto', {
+  value: {
+    randomUUID: () => crypto.randomUUID(),
+  },
+});
