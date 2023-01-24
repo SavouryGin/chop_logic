43e7fa885d49fea5dee7c3e254f5ee1ed9d56987
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-01-24 11:14:09 +0100

    ts-92: add draft dp sagas

---
+++ src/store/propositions/direct-proofs/sagas/paste-sub-proof.ts
@@ -0,0 +1,17 @@
+import { SagaIterator } from 'redux-saga';
+import { propositionsDPActions as actions } from 'store/propositions/direct-proofs/slice';
+import { errorsTexts } from 'texts';
+import { put, takeEvery } from 'redux-saga/effects';
+
+export function* pasteSubProofDPWatcher(): Generator {
+  yield takeEvery(actions.pasteSubProof, pasteSubProofDPSaga);
+}
+
+export function* pasteSubProofDPSaga(): SagaIterator {
+  yield put(actions.setUpFlag({ flag: 'isLoading', value: true }));
+  try {
+    console.log('cut saga');
+  } catch (error: unknown) {
+    yield put(actions.setError(errorsTexts.generalError));
+  }
+}
