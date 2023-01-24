de424cc39a0119cd1a108f3f3eae29cd94812e98
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-01-24 11:16:39 +0100

    ts-92: add dp draft sagas

--- src/store/propositions/natural-proofs/sagas/cut-sub-proof.ts
+++ src/store/propositions/natural-proofs/sagas/cut-sub-proof.ts
@@ -3,13 +3,13 @@ import { propositionsDPActions as actions } from 'store/propositions/direct-proo
 import { errorsTexts } from 'texts';
 import { put, takeEvery } from 'redux-saga/effects';
 
-export function* pasteSubProofDPWatcher(): Generator {
-  yield takeEvery(actions.pasteSubProof, pasteSubProofDPSaga);
+export function* cutSubProofNPWatcher(): Generator {
+  yield takeEvery(actions.cutSubProof, cutSubProofNPSaga);
 }
 
-export function* pasteSubProofDPSaga(): SagaIterator {
+export function* cutSubProofNPSaga(): SagaIterator {
   try {
-    console.log('pasteSubProofDPSaga');
+    console.log('cutSubProofNPSaga');
   } catch (error: unknown) {
     yield put(actions.setError(errorsTexts.generalError));
   }
