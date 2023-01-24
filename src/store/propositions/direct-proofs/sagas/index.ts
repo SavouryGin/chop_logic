637f31b585cf96ede799a1567203ca26b1e6a259
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-01-24 11:14:09 +0100

    ts-92: add draft dp sagas

--- src/store/propositions/direct-proofs/sagas/index.ts
+++ src/store/propositions/direct-proofs/sagas/index.ts
@@ -1,10 +1,18 @@
 import { all } from 'redux-saga/effects';
+import { cutSubProofDPWatcher } from './cut-sub-proof';
 import { deleteDirectProofStepsWatcher } from './delete-steps';
 import { exportDPToXMLWatcher } from './export-to-xml';
 import { importDPFromXMLWatcher } from './import-from-xml';
+import { pasteSubProofDPWatcher } from './paste-sub-proof';
 
 function* propositionsDPSagas(): Generator {
-  yield all([deleteDirectProofStepsWatcher(), exportDPToXMLWatcher(), importDPFromXMLWatcher()]);
+  yield all([
+    deleteDirectProofStepsWatcher(),
+    exportDPToXMLWatcher(),
+    importDPFromXMLWatcher(),
+    cutSubProofDPWatcher(),
+    pasteSubProofDPWatcher(),
+  ]);
 }
 
 export default propositionsDPSagas;
