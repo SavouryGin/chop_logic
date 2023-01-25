8a5dff244c6e12a0199179e4878f744f4d833e29
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-01-25 16:28:46 +0100

    ts-92: update pasteSubProofDPSaga()

--- src/store/propositions/direct-proofs/sagas/paste-sub-proof.ts
+++ src/store/propositions/direct-proofs/sagas/paste-sub-proof.ts
@@ -1,7 +1,9 @@
+import { DirectProofsTableItem } from '../interfaces';
 import { SagaIterator } from 'redux-saga';
 import { propositionsDPActions as actions } from 'store/propositions/direct-proofs/slice';
 import { errorsTexts } from 'texts';
-import { put, takeEvery } from 'redux-saga/effects';
+import { put, select, takeEvery } from 'redux-saga/effects';
+import { propositionsDPSelectors as selectors } from 'store/propositions/direct-proofs/selectors';
 
 export function* pasteSubProofDPWatcher(): Generator {
   yield takeEvery(actions.pasteSubProof, pasteSubProofDPSaga);
@@ -10,6 +12,8 @@ export function* pasteSubProofDPWatcher(): Generator {
 export function* pasteSubProofDPSaga(): SagaIterator {
   try {
     console.log('pasteSubProofDPSaga');
+    const clipboardData: DirectProofsTableItem[] = yield select(selectors.getClipboardData);
+    console.log('clipboardData', clipboardData);
   } catch (error: unknown) {
     yield put(actions.setError(errorsTexts.generalError));
   }
