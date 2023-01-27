315e945c8c9981fbf2ef081d0493a316feef15a4
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-01-27 15:33:05 +0100

    ts-92: add Spinner tests

---
+++ src/components/spinner/__tests__/spinner.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Spinner from 'components/spinner';
+import renderWithRedux from 'helpers/test-utils/render-with-redux';
+import { combineReducers } from '@reduxjs/toolkit';
+import { settingsInitialState, settingsSlice } from 'store/settings';
+
+describe('Spinner tests:', () => {
+  const mockedReducer = combineReducers({
+    settings: settingsSlice.reducer,
+  });
+
+  const mockedState = {
+    settings: settingsInitialState,
+  };
+
+  it('should match the snapshot', () => {
+    const { asFragment } = renderWithRedux(<Spinner />, mockedReducer, mockedState);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
