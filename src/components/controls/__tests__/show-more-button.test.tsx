284f3c92922db4d4fb048267b33ad54cc60ce8ec
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-01-28 15:59:30 +0100

    ts-92: ShowMoreButton component tests

---
+++ src/components/controls/__tests__/show-more-button.test.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ShowMoreButton from '../show-more-button';
+import renderWithRedux from 'helpers/test-utils/render-with-redux';
+import userEvent from '@testing-library/user-event';
+import { combineReducers } from '@reduxjs/toolkit';
+import { screen } from '@testing-library/react';
+import { settingsInitialState, settingsSlice } from 'store/settings';
+
+describe('ShowMoreButton component:', () => {
+  const testProps = {
+    onClick: jest.fn(),
+    isOpened: true,
+  };
+
+  const mockedReducer = combineReducers({
+    settings: settingsSlice.reducer,
+  });
+
+  const mockedState = {
+    settings: settingsInitialState,
+  };
+
+  it('renders the button element with the default type', () => {
+    renderWithRedux(<ShowMoreButton {...testProps} />, mockedReducer, mockedState);
+    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toHaveProperty('type', 'button');
+  });
+
+  it('has the passed class names', () => {
+    renderWithRedux(
+      <ShowMoreButton {...testProps} className={{ 'test-class-1': true, 'test-class-2': false, 'test-class-3': true }} />,
+      mockedReducer,
+      mockedState,
+    );
+    expect(screen.getByRole('button')).toHaveClass('show-more-button', 'test-class-1', 'test-class-3', 'chop-icon__caret-up');
+  });
+
+  it('fires the onClick handler', async () => {
+    const onClick = jest.fn();
+    renderWithRedux(<ShowMoreButton {...testProps} onClick={onClick} />, mockedReducer, mockedState);
+    const btn = screen.getByRole('button');
+    await userEvent.click(btn);
+    expect(onClick).toHaveBeenCalledTimes(1);
+    await userEvent.dblClick(btn);
+    expect(onClick).toHaveBeenCalledTimes(3);
+  });
+
+  it('could be disabled', () => {
+    renderWithRedux(<ShowMoreButton {...testProps} isDisabled={true} />, mockedReducer, mockedState);
+    expect(screen.getByRole('button')).toBeDisabled();
+  });
+});
