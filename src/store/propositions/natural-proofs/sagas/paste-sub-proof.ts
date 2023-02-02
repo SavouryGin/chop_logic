a5bab1e052fb0d34bc6d3b3ff7d431261602bd8a
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-02-02 09:04:03 +0100

    ts-92: Implement pasteSubProofNPSaga()

--- src/store/propositions/natural-proofs/sagas/paste-sub-proof.ts
+++ src/store/propositions/natural-proofs/sagas/paste-sub-proof.ts
@@ -1,7 +1,9 @@
+import { NaturalProofsTableItem } from '../interfaces';
 import { SagaIterator } from 'redux-saga';
 import { propositionsNPActions as actions } from 'store/propositions/natural-proofs';
 import { errorsTexts } from 'texts';
-import { put, takeEvery } from 'redux-saga/effects';
+import { put, select, takeEvery } from 'redux-saga/effects';
+import { propositionsNPSelectors as selectors } from 'store/propositions/natural-proofs/selectors';
 
 export function* pasteSubProofNPWatcher(): Generator {
   yield takeEvery(actions.pasteSubProof, pasteSubProofNPSaga);
@@ -9,7 +11,12 @@ export function* pasteSubProofNPWatcher(): Generator {
 
 export function* pasteSubProofNPSaga(): SagaIterator {
   try {
-    console.log('pasteSubProofDPSaga');
+    const tableItems: NaturalProofsTableItem[] = yield select(selectors.getTableData);
+    const clipboardData: NaturalProofsTableItem[] = yield select(selectors.getClipboardData);
+    const mergedData = [...tableItems, ...clipboardData].map((item, index) => ({ ...item, step: index + 1 }));
+
+    yield put(actions.setTableData(mergedData));
+    yield put(actions.setClipboardData([]));
   } catch (error: unknown) {
     yield put(actions.setError(errorsTexts.generalError));
   }
