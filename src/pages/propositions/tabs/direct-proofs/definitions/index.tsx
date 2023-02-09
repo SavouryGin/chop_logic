36c77ec815ba71cece40ac189128998ce0481f5c
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-02-09 10:06:06 +0100

    ts-94: modify DirectProofsPropositionDefinitions

--- src/pages/propositions/tabs/direct-proofs/definitions/index.tsx
+++ src/pages/propositions/tabs/direct-proofs/definitions/index.tsx
@@ -1,8 +1,21 @@
+import DPDefinitionsEn from 'texts/propositions/en/dp-definitions-en';
+import DPDefinitionsRu from 'texts/propositions/ru/dp-definitions-ru';
 import React from 'react';
+import { settingsSelectors } from 'store/settings/selectors';
+import { useAppSelector } from 'hooks';
 import './styles.scss';
 
 const DirectProofsPropositionDefinitions = (): React.ReactElement => {
-  return <div>Definitions</div>;
+  const language = useAppSelector(settingsSelectors.getLanguage);
+
+  switch (language) {
+    case 'ru': {
+      return <DPDefinitionsRu />;
+    }
+    default: {
+      return <DPDefinitionsEn />;
+    }
+  }
 };
 
 export default DirectProofsPropositionDefinitions;
