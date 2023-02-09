3c7c151172eb176d9bf4a2ccf4c51da5351d1008
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-02-09 09:52:49 +0100

    ts-94: add draft ru text components

---
+++ src/texts/propositions/ru/dp-definitions-ru/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.scss';
+
+const DPDefinitionsRu = (): React.ReactElement => {
+  return <section className='dp-definitions-ru'></section>;
+};
+
+export default DPDefinitionsRu;
