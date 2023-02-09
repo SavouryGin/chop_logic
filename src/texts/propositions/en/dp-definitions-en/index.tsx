aafd3764ae3414faa3e87bbfab91cfb807dd846c
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-02-09 09:54:08 +0100

    ts-94: add draft en text components

---
+++ src/texts/propositions/en/dp-definitions-en/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.scss';
+
+const DPDefinitionsEn = (): React.ReactElement => {
+  return <section className='dp-definitions-en'></section>;
+};
+
+export default DPDefinitionsEn;
