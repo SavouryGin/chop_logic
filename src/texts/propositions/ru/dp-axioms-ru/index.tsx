3c7d035a288486eefbe593ce0414a8673467b1a8
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-02-09 09:52:49 +0100

    ts-94: add draft ru text components

---
+++ src/texts/propositions/ru/dp-axioms-ru/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.scss';
+
+const DPAxiomsRu = (): React.ReactElement => {
+  return <section className='dp-axioms-ru'></section>;
+};
+
+export default DPAxiomsRu;
