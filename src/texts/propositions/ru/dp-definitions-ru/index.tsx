c507c0b156b0489d05f826512a26035c62760502
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-02-11 13:25:44 +0100

    ts-94: install react-latex

--- src/texts/propositions/ru/dp-definitions-ru/index.tsx
+++ src/texts/propositions/ru/dp-definitions-ru/index.tsx
@@ -1,8 +1,15 @@
+import Latex from 'react-latex';
 import React from 'react';
 import './styles.scss';
 
 const DPDefinitionsRu = (): React.ReactElement => {
-  return <section className='dp-definitions-ru'>DPDefinitionsRu</section>;
+  const test = `$$\\lambda (F(A_{1}, A_{2}, ..., A_{n}) \\leftrightarrow G(A_{1}, A_{2}, ..., A_{n})) = 1$$`;
+
+  return (
+    <section className='dp-definitions-ru'>
+      <Latex displayMode={true}>{test}</Latex>
+    </section>
+  );
 };
 
 export default DPDefinitionsRu;
