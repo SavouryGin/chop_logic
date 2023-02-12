9167eaf9182b3a437e7edc6af3abecabeb22cb3c
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-02-12 12:52:00 +0100

    ts-94: add proofDefinitions

--- src/texts/propositions/ru/dp-definitions-ru/index.tsx
+++ src/texts/propositions/ru/dp-definitions-ru/index.tsx
@@ -8,6 +8,14 @@ const DPDefinitionsRu = (): React.ReactElement => {
   const Ri = `$$R_{i}$$`;
   const k = `$$k$$`;
   const F = `$$F$$`;
+  const F1Fn = `$$F_{1}, F_{2}, ..., F_{n}$$`;
+  const G = `$$G$$`;
+  const Delta = `$$\\Delta$$`;
+  const Fi = `$$F_{i}$$`;
+  const Fn = `$$F_{n}$$`;
+  const DeltaToG = `$$\\Delta \\vdash G.$$`;
+  const DeltaToNothing = `$$\\Delta~=~\\varnothing$$`;
+  const toG = `$$\\vdash G$$`;
 
   const formalTheoryDefinition = (
     <div>
@@ -32,7 +40,33 @@ const DPDefinitionsRu = (): React.ReactElement => {
     </div>
   );
 
-  return <section className='dp-definitions-ru'>{formalTheoryDefinition}</section>;
+  const proofDefinitions = (
+    <div>
+      <dfn>Выводом</dfn> формулы <Latex>{G}</Latex> из множества формул <Latex>{Delta}</Latex> называется такая конечная последовательность
+      формул <Latex>{F1Fn}</Latex>, в которой каждая формула <Latex>{Fi}</Latex> является либо
+      <ol>
+        <li>формулой из множества, либо</li>
+        <li>аксиомой формальной теории, либо</li>
+        <li>получена из предыдущих формул последовательности по правилу вывода;</li>
+      </ol>
+      при этом последняя формула <Latex>{Fn}</Latex> совпадает с <Latex>{G}</Latex>. Отношение выводимости обозначается следующим образом:{' '}
+      <Latex>{DeltaToG}</Latex>
+      Эта запись читается как "формула <Latex>{G}</Latex> выводима из множества формул <Latex>{Delta}</Latex>" или "<Latex>{Delta}</Latex>{' '}
+      выводит <Latex>{G}</Latex>".<br></br> Элементы <Latex>{Delta}</Latex> называются <em>посылками</em>. Если{' '}
+      <Latex>{DeltaToNothing}</Latex>, то есть не содержит ни одной посылки, то говорят, что <Latex>{G}</Latex> выводима из аксиом, или{' '}
+      <Latex>{G}</Latex> доказуема в данной формальной теории. Саму последовательность <Latex>{F1Fn}</Latex> называют{' '}
+      <em>доказательством</em> формулы <Latex>{G}</Latex>. Запись <Latex>{toG}</Latex>
+      означает: "<Latex>{G}</Latex> доказана" или "<Latex>{G}</Latex> - теорема".
+    </div>
+  );
+
+  return (
+    <section className='dp-definitions-ru'>
+      {formalTheoryDefinition}
+      <br></br>
+      {proofDefinitions}
+    </section>
+  );
 };
 
 export default DPDefinitionsRu;
