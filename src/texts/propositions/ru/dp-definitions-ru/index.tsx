a78fc4d3cbf2d3db4b9bb3c946e9386a4ddb9517
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-02-12 12:30:06 +0100

    ts-94: add formalTheoryDefinition ru

--- src/texts/propositions/ru/dp-definitions-ru/index.tsx
+++ src/texts/propositions/ru/dp-definitions-ru/index.tsx
@@ -3,13 +3,36 @@ import React from 'react';
 import './styles.scss';
 
 const DPDefinitionsRu = (): React.ReactElement => {
-  const test = `$$\\lambda (F(A_{1}, A_{2}, ..., A_{n}) \\leftrightarrow G(A_{1}, A_{2}, ..., A_{n})) = 1$$`;
+  const T = `$$\\mathfrak{T}$$`;
+  const R1Rn = `$\\{R_{1}, R_{2}, ..., R_{n}\\}$`;
+  const Ri = `$$R_{i}$$`;
+  const k = `$$k$$`;
+  const F = `$$F$$`;
 
-  return (
-    <section className='dp-definitions-ru'>
-      <Latex displayMode={true}>{test}</Latex>
-    </section>
+  const formalTheoryDefinition = (
+    <div>
+      <dfn>Формальная (аксиоматическая) теория</dfn> <Latex>{T}</Latex> считается определенной, если соблюдены следующие требования:
+      <ul>
+        <li>
+          задано некоторое счетное множество символов теории <Latex>{T}</Latex>, в качестве которых могут быть выбраны произвольные объекты
+          (не обязательно лингвистические). Конечные последовательности символов называются <em>выражениями</em> <Latex>{T}</Latex>;
+        </li>
+        <li>
+          задано счетное подмножество выражений, именуемых <em>формулами</em> теории <Latex>{T}</Latex>. Если имеется возможность выяснить,
+          является ли данная формула аксиомой, то теория <Latex>{T}</Latex> называется <em>аксиоматической теорией</em>;
+        </li>
+        <li>
+          задано множество <Latex>{R1Rn}</Latex> отношений между формулами, которые называются <em>правилами вывода</em>. Причем для каждого{' '}
+          <Latex>{Ri}</Latex> существует целое положительное число <Latex>{k}</Latex> такое, что для каждого множества, состоящего из{' '}
+          <Latex>{k}</Latex> формул, и для каждой формулы <Latex>{F}</Latex> эффективно решается вопрос о том, состоят ли данные{' '}
+          <Latex>{k}</Latex> формул в отношении <Latex>{Ri}</Latex> с формулой <Latex>{F}</Latex> или нет. Если состоят, то{' '}
+          <Latex>{F}</Latex> называется непосредственным следствием из <Latex>{k}</Latex> формул по правилу <Latex>{Ri}</Latex>.
+        </li>
+      </ul>
+    </div>
   );
+
+  return <section className='dp-definitions-ru'>{formalTheoryDefinition}</section>;
 };
 
 export default DPDefinitionsRu;
