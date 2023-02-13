20146b3ad82b78392bb9a59df6104731df04ce6c
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-02-13 09:27:48 +0100

    ts-94: extract latexExpressions

---
+++ src/texts/propositions/latex-expressions/index.ts
@@ -0,0 +1,17 @@
+const latexExpressions = {
+  T: `$$\\mathfrak{T}$$`,
+  R1Rn: `$\\{R_{1}, R_{2}, ..., R_{n}\\}$`,
+  Ri: `$$R_{i}$$`,
+  k: `$$k$$`,
+  F: `$$F$$`,
+  F1Fn: `$$F_{1}, F_{2}, ..., F_{n}$$`,
+  G: `$$G$$`,
+  Delta: `$$\\Delta$$`,
+  Fi: `$$F_{i}$$`,
+  Fn: `$$F_{n}$$`,
+  DeltaToG: `$$\\Delta \\vdash G.$$`,
+  DeltaToNothing: `$$\\Delta~=~\\varnothing$$`,
+  toG: `$$\\vdash G$$`,
+};
+
+export default Object.freeze(latexExpressions);
