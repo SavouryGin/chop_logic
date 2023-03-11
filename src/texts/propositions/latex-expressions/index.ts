19db1b609c10bd03440a0b70ed14a2b6f8871b52
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-03-11 10:39:16 +0100

    ts-97: add latexExpressions

--- src/texts/propositions/latex-expressions/index.ts
+++ src/texts/propositions/latex-expressions/index.ts
@@ -15,6 +15,14 @@ const latexExpressions = {
   IC: `$$\\Phi \\rightarrow (\\Psi \\rightarrow \\Phi)$$`,
   ID: `$$(\\Phi \\rightarrow (\\Psi \\rightarrow \\Chi)) \\rightarrow ((\\Phi \\rightarrow \\Psi) \\rightarrow (\\Phi \\rightarrow \\Chi))$$`,
   CR: `$$(\\neg \\Psi \\rightarrow \\neg \\Phi) \\rightarrow ((\\neg \\Psi \\rightarrow \\Phi) \\rightarrow \\Psi)$$`,
+  FtoG: `$$F \\rightarrow G$$`,
+  FandG: `$$F \\land G$$`,
+  ForG: `$$F \\lor G$$`,
+  notF: `$$\\neg F$$`,
+  FtoNotG: `$$F \\rightarrow \\neg G$$`,
+  FtoH: `$$F \\rightarrow H$$`,
+  GtoH: `$$G \\rightarrow H$$`,
+  H: `$$H$$`,
 };
 
 export default Object.freeze(latexExpressions);
