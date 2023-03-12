a1e69fda17578038959c252bca9adc7c7058427f
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-03-12 11:54:32 +0100

    ts-97: add table components for rules

--- src/texts/propositions/en/np-rules-en/index.tsx
+++ src/texts/propositions/en/np-rules-en/index.tsx
@@ -3,7 +3,34 @@ import formatClass from 'helpers/formatters/format-class-name';
 import { CommonProps } from 'types';
 
 const NPRulesEn = (props: CommonProps): React.ReactElement => {
-  return <div className={formatClass([props.className])}>Rules En</div>;
+  return (
+    <table className={formatClass([props.className])}>
+      <tr>
+        <th>Introduction rules</th>
+        <th>Elimination rules</th>
+      </tr>
+      <tr>
+        <td>Conjunction Introduction (CI)</td>
+        <td>Conjunction Elimination (CE)</td>
+      </tr>
+      <tr>
+        <td>Disjunction Introduction (DI)</td>
+        <td>Disjunction Elimination (DE)</td>
+      </tr>
+      <tr>
+        <td>Implication Introduction (II)</td>
+        <td>Implication Elimination (IE)</td>
+      </tr>
+      <tr>
+        <td>Equivalence Introduction (EI)</td>
+        <td>Equivalence Elimination (EE)</td>
+      </tr>
+      <tr>
+        <td>Negation Introduction (NI)</td>
+        <td>Negation Elimination (NE)</td>
+      </tr>
+    </table>
+  );
 };
 
 export default NPRulesEn;
--- src/texts/propositions/ru/np-rules-ru/index.tsx
+++ src/texts/propositions/ru/np-rules-ru/index.tsx
@@ -1,9 +1,37 @@
+// import Latex from 'react-latex';
 import React from 'react';
 import formatClass from 'helpers/formatters/format-class-name';
 import { CommonProps } from 'types';
 
 const NPRulesRu = (props: CommonProps): React.ReactElement => {
-  return <div className={formatClass([props.className])}>Rules Ru</div>;
+  return (
+    <table className={formatClass([props.className])}>
+      <tr>
+        <th>Правила введения связок</th>
+        <th>Правила удаления связок</th>
+      </tr>
+      <tr>
+        <td>Введение конъюнкции (ВК)</td>
+        <td>Удаление конъюнкции (УК)</td>
+      </tr>
+      <tr>
+        <td>Введение дизъюнкции (ВД)</td>
+        <td>Удаление дизъюнкции (УД)</td>
+      </tr>
+      <tr>
+        <td>Введение импликации (ВИ)</td>
+        <td>Удаление импликации (УИ)</td>
+      </tr>
+      <tr>
+        <td>Введение эквивалентности (ВЭ)</td>
+        <td>Удаление эквивалентности (УЭ)</td>
+      </tr>
+      <tr>
+        <td>Введение отрицания (ВО)</td>
+        <td>Удаление отрицания (УО)</td>
+      </tr>
+    </table>
+  );
 };
 
 export default NPRulesRu;
