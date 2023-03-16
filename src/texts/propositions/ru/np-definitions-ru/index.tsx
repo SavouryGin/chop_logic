da86eee0b908bfc41b69e83679cc9795657f7833
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-03-16 10:14:19 +0100

    ts-97: add draft NP definitions components

---
+++ src/texts/propositions/ru/np-definitions-ru/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CommonProps } from 'types';
+
+const NPDefinitionsRu = (props: CommonProps): React.ReactElement => {
+  console.log(props);
+
+  return <section>Определения</section>;
+};
+
+export default NPDefinitionsRu;
