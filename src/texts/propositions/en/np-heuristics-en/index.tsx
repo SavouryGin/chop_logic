9d6e4bf32076f0383784dcdf9e35054f27d4c8b8
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-03-24 10:13:39 +0100

    ts-99: refactor heuristics components

--- src/texts/propositions/en/np-heuristics-en/index.tsx
+++ src/texts/propositions/en/np-heuristics-en/index.tsx
@@ -8,8 +8,8 @@ const NPHeuristicsEn = (props: CommonProps): React.ReactElement => {
   return (
     <ol className={formatClass([props.className])}>
       <li>
-        heuristic: if the goal is to prove a formula like <Latex>{latex.FtoG}</Latex>, you should enter the assumption{' '}
-        <Latex>{latex.F}</Latex> and in the the sub-proof try to derive <Latex>{latex.G}</Latex>. Then you have to use the{' '}
+        If the goal is to prove a formula like <Latex>{latex.FtoG}</Latex>, you should enter the assumption <Latex>{latex.F}</Latex> and in
+        the the sub-proof try to derive <Latex>{latex.G}</Latex>. Then you have to use the{' '}
         <abbr
           title='Implication
         Introduction'
@@ -19,30 +19,30 @@ const NPHeuristicsEn = (props: CommonProps): React.ReactElement => {
         rule on the first and last formulas of the sub-proof to get <Latex>{latex.FtoG}</Latex>.
       </li>
       <li>
-        heuristic: if the goal is to prove a formula like <Latex>{latex.FandG}</Latex>, you should first derive the formula
+        If the goal is to prove a formula like <Latex>{latex.FandG}</Latex>, you should first derive the formula
         <Latex>{latex.F}</Latex> and then the formula <Latex>{latex.G}</Latex>. If the derivation of both formulas succeeds, you need to
         apply the <abbr title='Conjunction Introduction'>CI</abbr> rule on them to get <Latex>{latex.FandG}</Latex>.
       </li>
       <li>
-        heuristic: if the goal is to prove the formula like <Latex>{latex.ForG}</Latex>, then at least one of the formulas
+        If the goal is to prove the formula like <Latex>{latex.ForG}</Latex>, then at least one of the formulas
         <Latex>{latex.F}</Latex> or <Latex>{latex.G}</Latex> must be derived. Once the derivation of one of the formulas is obtained, you
         can get a disjunction of that formula with any other formula by the <abbr title='Disjunction Introduction'>DI</abbr> rule.
       </li>
       <li>
-        heuristic: if the goal is to prove a formula like <Latex>{latex.notF}</Latex>, you should introduce the assumtion{' '}
-        <Latex>{latex.F}</Latex> and derive a contradiction in the sub-proof, i.e., two formulas: <Latex>{latex.FtoG}</Latex> и{' '}
-        <Latex>{latex.FtoNotG}</Latex>. Once the contradiction is obtained, the <abbr title='Negation Introduction'>NI</abbr> rule will give
-        you the desired <Latex>{latex.notF}</Latex>.
+        If the goal is to prove a formula like <Latex>{latex.notF}</Latex>, you should introduce the assumtion <Latex>{latex.F}</Latex> and
+        derive a contradiction in the sub-proof, i.e., two formulas: <Latex>{latex.FtoG}</Latex> и <Latex>{latex.FtoNotG}</Latex>. Once the
+        contradiction is obtained, the <abbr title='Negation Introduction'>NI</abbr> rule will give you the desired{' '}
+        <Latex>{latex.notF}</Latex>.
       </li>
       <li>
-        heuristic: if a premise is given as <Latex>{latex.FtoG}</Latex> and the goal is to prove <Latex>{latex.G}</Latex>, you must first
-        derive <Latex>{latex.F}</Latex>. If the derivation succeeds, the formula <Latex>{latex.G}</Latex> can be obtained by the{' '}
+        If a premise is given as <Latex>{latex.FtoG}</Latex> and the goal is to prove <Latex>{latex.G}</Latex>, you must first derive{' '}
+        <Latex>{latex.F}</Latex>. If the derivation succeeds, the formula <Latex>{latex.G}</Latex> can be obtained by the{' '}
         <abbr title='Implication Elimination'>IE</abbr> rule.
       </li>
       <li>
-        heuristic: if a premise is given as <Latex>{latex.ForG}</Latex> and the goal is to derive <Latex>{latex.H}</Latex>, then you should
-        first prove <Latex>{latex.FtoH}</Latex> and then <Latex>{latex.GtoH}</Latex>. On these two formulas and the original disjunctive
-        premise, use the <abbr title='Disjunction Elimination'>DE</abbr> rule to get <Latex>{latex.H}</Latex>.
+        If a premise is given as <Latex>{latex.ForG}</Latex> and the goal is to derive <Latex>{latex.H}</Latex>, then you should first prove{' '}
+        <Latex>{latex.FtoH}</Latex> and then <Latex>{latex.GtoH}</Latex>. On these two formulas and the original disjunctive premise, use
+        the <abbr title='Disjunction Elimination'>DE</abbr> rule to get <Latex>{latex.H}</Latex>.
       </li>
     </ol>
   );
