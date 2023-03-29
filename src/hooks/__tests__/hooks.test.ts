0292990896c6c53f291f0817d2a35bcdc5f02f24
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-03-29 21:28:03 +0200

    ts-100: test useImplicationCreationPreview() hook

--- src/hooks/__tests__/hooks.test.ts
+++ src/hooks/__tests__/hooks.test.ts
@@ -1,6 +1,6 @@
 import { renderHook } from '@testing-library/react';
+import { useImplicationCreationPreview, usePropositionalFormulaPreview } from 'hooks/propositions';
 import { useMount } from 'hooks/common';
-import { usePropositionalFormulaPreview } from 'hooks/propositions';
 
 describe('Common hooks tests:', () => {
   it('useMount() should return isMounted value', () => {
@@ -9,9 +9,29 @@ describe('Common hooks tests:', () => {
     expect(result.current).toBeTruthy();
   });
 
+  it('useMount() should return isMounted value', () => {
+    const { result } = renderHook(() => useMount(false, 300));
+
+    expect(result.current).toBeFalsy();
+  });
+
   it('usePropositionalFormulaPreview() should return isMounted value', () => {
     const { result } = renderHook(() => usePropositionalFormulaPreview('p'));
 
     expect(result.current).toEqual([{ input: 'P', position: 0, representation: 'P', type: 'variable' }]);
+  });
+
+  it('useImplicationCreationPreview() should return isMounted value', () => {
+    const { result } = renderHook(() => useImplicationCreationPreview('p', 'r'));
+
+    expect(result.current).toEqual([
+      { input: 'P', position: 0, representation: 'P', type: 'variable' },
+      { input: '=>', position: 1, representation: '⇒', type: 'operator' },
+      { input: '(', position: 2, representation: '(', type: 'parentheses' },
+      { input: 'R', position: 3, representation: 'R', type: 'variable' },
+      { input: '=>', position: 4, representation: '⇒', type: 'operator' },
+      { input: 'P', position: 5, representation: 'P', type: 'variable' },
+      { input: ')', position: 6, representation: ')', type: 'parentheses' },
+    ]);
   });
 });
