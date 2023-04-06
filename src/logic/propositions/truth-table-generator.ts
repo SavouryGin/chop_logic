26b08ce2a49a1d67401b7cd66dfcf609660dcdd8
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-04-06 19:36:06 +0200

    ts-101: add draft generateColumnsFromFormula

--- src/logic/propositions/truth-table-generator.ts
+++ src/logic/propositions/truth-table-generator.ts
@@ -1,9 +1,31 @@
 import { PropositionalFormula, TableColumnProps } from 'types';
+import { PropositionalOperator } from 'enums';
 
 const generateColumnsFromFormula = (formula: PropositionalFormula): TableColumnProps[] => {
-  console.log(formula);
+  const list: TableColumnProps[] = [];
 
-  return [];
+  if (formula.operator === PropositionalOperator.Var) {
+    list.push(createVariableColumn(formula.values as string));
+  }
+
+  // switch(formula.operator) {
+  //   case(PropositionalOperator.And): {
+  //   }
+  // }
+
+  return list;
+};
+
+const createVariableColumn = (value: string): TableColumnProps => {
+  const titleValue = value.toUpperCase();
+
+  return {
+    field: value,
+    title: {
+      en: titleValue,
+      ru: titleValue,
+    },
+  };
 };
 
 const truthTableGenerator = {
--- src/store/propositions/truth-tables/sagas/generate-truth-table.ts
+++ src/store/propositions/truth-tables/sagas/generate-truth-table.ts
@@ -1,5 +1,6 @@
 import converter from 'logic/propositions/converter';
 import errorsTexts from 'texts/propositions/elements';
+import truthTableGenerator from 'logic/propositions/truth-table-generator';
 import { PropositionalExpression, PropositionalFormula } from 'types';
 import { SagaIterator } from 'redux-saga';
 import { truthTablesActions as actions } from 'store/propositions/truth-tables';
@@ -16,6 +17,8 @@ export function* generateTruthTableSaga(action: { payload: { input: string } }):
     const formula: PropositionalFormula = yield call(converter.convertExpressionToFormula, expression);
 
     console.log('Saga', formula);
+    const columns = truthTableGenerator.generateColumnsFromFormula(formula);
+    console.log(columns);
   } catch (error: unknown) {
     yield put(actions.setError(errorsTexts.generalError));
   }
