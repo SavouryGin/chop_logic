07faac95949570dce7e48ed7dc1794238f16413a
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-04-26 18:11:53 +0200

    ts-102: add createBinaryColumn() test

--- src/logic/propositions/__tests__/truth-tables-generator.test.ts
+++ src/logic/propositions/__tests__/truth-tables-generator.test.ts
@@ -1,5 +1,6 @@
 import generator from '../truth-table-generator';
 import mocks from '__mocks__/data/propositions/formulas-items';
+import { LogicalSymbolHexCode, PropositionalOperator } from 'enums';
 
 describe('Truth Table Generator tests', () => {
   it('createVariableColumn() method returns a correct TruthTableColumn', () => {
@@ -37,6 +38,40 @@ describe('Truth Table Generator tests', () => {
       operator: 'NOT',
       title: { en: '¬ ¬ P', ru: '¬ ¬ P' },
       operands: ['¬ P'],
+    });
+  });
+
+  it('createBinaryColumn() method returns a correct TruthTableColumn', () => {
+    const column = generator.createBinaryColumn({
+      firstOperand: mocks.propositionalAtom,
+      secondOperand: mocks.propositionalAtom,
+      depth: 0,
+      operator: PropositionalOperator.And,
+      symbol: LogicalSymbolHexCode.Conjunction,
+    });
+
+    expect(column).toEqual({
+      field: 'P ∧ P',
+      depth: 0,
+      operator: 'AND',
+      title: { en: 'P ∧ P', ru: 'P ∧ P' },
+      operands: ['P', 'P'],
+    });
+
+    const column2 = generator.createBinaryColumn({
+      firstOperand: mocks.firstSubFormula,
+      secondOperand: mocks.secondSubFormula,
+      depth: 1,
+      operator: PropositionalOperator.Implies,
+      symbol: LogicalSymbolHexCode.Implication,
+    });
+
+    expect(column2).toEqual({
+      field: '¬ P ⇒ ( P ∧ Q ) ⇒ ¬ P',
+      depth: 1,
+      operator: 'IMPLIES',
+      title: { en: '¬ P ⇒ ( P ∧ Q ) ⇒ ¬ P', ru: '¬ P ⇒ ( P ∧ Q ) ⇒ ¬ P' },
+      operands: ['¬ P', '( P ∧ Q ) ⇒ ¬ P'],
     });
   });
 });
