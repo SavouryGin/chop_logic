51efd7d9a98f54a3b110aa512427abc7d33ff984
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-06-05 14:16:55 +0200

    ts-108: add error popup

--- src/pages/truth-tables/index.tsx
+++ src/pages/truth-tables/index.tsx
@@ -1,12 +1,24 @@
+import ErrorPopup from 'components/error-popup';
 import React from 'react';
 import TruthTableInput from './components/input';
 import TruthTablesTable from './components/table';
+import truthTablesSelectors from 'store/propositions/truth-tables/selectors';
+import { truthTablesActions } from 'store/propositions/truth-tables';
+import { useAppDispatch, useAppSelector } from 'hooks';
 
 const TruthTables = (): React.ReactElement => {
+  const error = useAppSelector(truthTablesSelectors.error);
+  const dispatch = useAppDispatch();
+
+  const resetError = () => {
+    dispatch(truthTablesActions.setError(null));
+  };
+
   return (
     <div className='truth-tables'>
       <TruthTableInput />
       <TruthTablesTable />
+      <ErrorPopup onClose={resetError} error={error} />
     </div>
   );
 };
