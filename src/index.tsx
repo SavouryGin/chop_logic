1baee4bd5d2e754f11430c9cb9ff5d0e9a785074
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-07-20 10:58:33 +0200

    ts-113: import KaTeX styles into the project assets

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import { createRoot } from 'react-dom/client';
 import 'styles/icons.scss';
 import 'styles/reset.scss';
 
+// KatTeX dependency
+import 'styles/latex/KaTeX-0.9.0.min.scss';
+
 // React 18 root API
 const rootContainer = document.getElementById('root');
 if (!rootContainer) {
