700d2d17282b4f299756b744897019f3909e06df
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-07-23 19:58:44 +0200

    ts-113: add operand columns

---
+++ src/pages/propositions/tabs/truth-tables/operands/columns.ts
@@ -0,0 +1,20 @@
+import { TableColumn } from 'types';
+
+export const TRUTH_TABLES_OPERANDS_COLUMNS: TableColumn[] = [
+  { field: 'f0', title: { ru: 'f0', en: 'f0' } },
+  { field: 'f1', title: { ru: 'f1', en: 'f1' } },
+  { field: 'f2', title: { ru: 'f2', en: 'f2' } },
+  { field: 'f3', title: { ru: 'f3', en: 'f3' } },
+  { field: 'f4', title: { ru: 'f4', en: 'f4' } },
+  { field: 'f5', title: { ru: 'f5', en: 'f5' } },
+  { field: 'f6', title: { ru: 'f6', en: 'f6' } },
+  { field: 'f7', title: { ru: 'f7', en: 'f7' } },
+  { field: 'f8', title: { ru: 'f8', en: 'f8' } },
+  { field: 'f9', title: { ru: 'f9', en: 'f9' } },
+  { field: 'f10', title: { ru: 'f10', en: 'f10' } },
+  { field: 'f11', title: { ru: 'f11', en: 'f11' } },
+  { field: 'f12', title: { ru: 'f12', en: 'f12' } },
+  { field: 'f13', title: { ru: 'f13', en: 'f13' } },
+  { field: 'f14', title: { ru: 'f14', en: 'f14' } },
+  { field: 'f15', title: { ru: 'f0', en: 'f15' } },
+];
---
+++ src/pages/propositions/tabs/truth-tables/operands/values.ts
@@ -0,0 +1,3 @@
+import { TableItem } from 'types';
+
+export const TRUTH_TABLES_OPERANDS_VALUES: TableItem[] = [];
