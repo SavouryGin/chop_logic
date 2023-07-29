b43722cea0038f8a8d1e97fb207d209a777d6c28
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-07-29 12:03:17 +0200

    ts-114: refactor TruthTablesLaws

--- src/pages/propositions/tabs/truth-tables/laws/index.tsx
+++ src/pages/propositions/tabs/truth-tables/laws/index.tsx
@@ -1,22 +1,26 @@
-import PropositionLawsEn from 'utils/texts/propositions/en/laws';
-import PropositionLawsRu from 'utils/texts/propositions/ru/laws';
+import Latex from 'react-latex';
 import React from 'react';
+import { PROPOSITIONAL_LAWS_LIST } from 'utils/texts/propositions/latex-expressions/laws-list';
 import { settingsSelectors } from 'store/settings/selectors';
 import { useAppSelector } from 'hooks';
 
-const TruthTablesLaws = (): React.ReactElement | null => {
+const TruthTablesLaws = (): React.ReactElement => {
   const language = useAppSelector(settingsSelectors.language);
 
-  switch (language) {
-    case 'ru': {
-      return <PropositionLawsRu />;
-    }
-    case 'en': {
-      return <PropositionLawsEn />;
-    }
-    default:
-      return null;
-  }
+  return (
+    <section>
+      <ol>
+        {PROPOSITIONAL_LAWS_LIST.map((item) => {
+          return (
+            <li key={item.id}>
+              <strong>{item.title[language]}</strong>
+              <Latex>{item.latexExpression}</Latex>
+            </li>
+          );
+        })}
+      </ol>
+    </section>
+  );
 };
 
 export default TruthTablesLaws;
